9554b74291bdc1ff2da65649ec0dd724a1a40da2
Lin Xiao <tsunamibad@qq.com>
2016-12-14 03:59:14 +0800

    Retrofit Https支持参考代码，文件管理类框架

--- retrofit-base/framework/src/main/java/com/linxiao/framework/net/RetrofitApiManager.java
+++ retrofit-base/framework/src/main/java/com/linxiao/framework/net/RetrofitApiManager.java
@@ -1,24 +1,26 @@
 package com.linxiao.framework.net;
 
+import android.support.annotation.NonNull;
+import android.util.Log;
+
 import com.linxiao.framework.BaseApplication;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.security.KeyManagementException;
 import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
-import java.util.ArrayList;
 
+import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManagerFactory;
 
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.CallAdapter;
 import retrofit2.Converter;
 import retrofit2.Retrofit;
@@ -31,8 +33,8 @@ public class RetrofitApiManager {
 
     private static final String TAG = RetrofitApiManager.class.getSimpleName();
 
-    public static RetrofitAPIBuilder buildClientAPI() {
-        return new RetrofitAPIBuilder();
+    public static RetrofitApiBuilder buildClientAPI() {
+        return new RetrofitApiBuilder();
     }
 
     protected static SSLSocketFactory getSSLSocketFactory(int[] certificates) {
@@ -61,44 +63,106 @@ public class RetrofitApiManager {
         return null;
     }
 
-    public static class RetrofitAPIBuilder {
+    protected static HostnameVerifier getHostnameVerifier(final String[] hostUrls) {
+
+        HostnameVerifier TRUSTED_VERIFIER = new HostnameVerifier() {
+
+            public boolean verify(String hostname, SSLSession session) {
+                boolean ret = false;
+                for (String host : hostUrls) {
+                    if (host.equalsIgnoreCase(hostname)) {
+                        ret = true;
+                    }
+                }
+                return ret;
+            }
+        };
+
+        return TRUSTED_VERIFIER;
+    }
+
+    @NonNull
+    public static Interceptor provideHttpReqInterceptor() {
+        return new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request.Builder builder = chain.request()
+                        .newBuilder()
+                        .addHeader("Content-Type", "application/json; charset=UTF-8")
+                        .addHeader("Accept-Encoding", "gzip, deflate")
+                        .addHeader("Connection", "keep-alive")
+                        .addHeader("Accept", "*/*");
+//                        .addHeader("User-Agent", GlobalConfig.USER_AGENT);
+                if (SessionManager.getSession() != null) {
+                    Log.d("Request-Cookie", SessionManager.getSession());
+                    builder.addHeader("Cookie", SessionManager.getSession());
+                }
+                Request request = builder.build();
+                Log.d(TAG, "request url = " + request.url().toString());
+                return chain.proceed(request);
+            }
+        };
+    }
+
+    public static class RetrofitApiBuilder {
 
         private Retrofit.Builder mRetrofitBuilder;
-        private ArrayList<Interceptor> interceptors;
+        private OkHttpClient.Builder okHttpClientBuilder;
 
-        public RetrofitAPIBuilder() {
+        public RetrofitApiBuilder() {
             mRetrofitBuilder = new Retrofit.Builder();
-            interceptors = new ArrayList<>();
+            okHttpClientBuilder = new OkHttpClient.Builder();
         }
 
-        public RetrofitAPIBuilder setServerUrl(String serverUrl) {
+        public RetrofitApiBuilder setServerUrl(String serverUrl) {
             mRetrofitBuilder.baseUrl(serverUrl);
             return this;
         }
 
-        public RetrofitAPIBuilder addInterceptor(Interceptor interceptor) {
-            interceptors.add(interceptor);
+        public RetrofitApiBuilder addCookieManager() {
+            okHttpClientBuilder.addInterceptor(new Interceptor() {
+                @Override
+                public Response intercept(Chain chain) throws IOException {
+                    Response response = chain.proceed(chain.request());
+                    //存入Session
+                    if (response.header("Set-Cookie") != null) {
+                        SessionManager.setSession(response.header("Set-Cookie"));
+                    }
+                    //刷新API调用时间
+                    SessionManager.setLastApiCallTime(System.currentTimeMillis());
+
+                    return response;
+                }
+            });
             return this;
         }
 
-        public RetrofitAPIBuilder setOkHttpClient(OkHttpClient okHttpClient) {
+        public RetrofitApiBuilder addCustomInterceptor(Interceptor interceptor) {
+            okHttpClientBuilder.addInterceptor(interceptor);
+            return this;
+        }
+
+        public RetrofitApiBuilder addHttpsSupport(int[] certificates, String[] hostUrls) {
+            okHttpClientBuilder.socketFactory(getSSLSocketFactory(certificates));
+            okHttpClientBuilder.hostnameVerifier(getHostnameVerifier(hostUrls));
+            return this;
+        }
+
+        public RetrofitApiBuilder setCustomOkHttpClient(OkHttpClient okHttpClient) {
             mRetrofitBuilder.client(okHttpClient);
             return this;
         }
 
-        public RetrofitAPIBuilder addCallAdapterFactory(CallAdapter.Factory factory) {
+        public RetrofitApiBuilder addCallAdapterFactory(CallAdapter.Factory factory) {
             mRetrofitBuilder.addCallAdapterFactory(factory);
             return this;
         }
 
-        public RetrofitAPIBuilder addConvertFactory(Converter.Factory factory) {
+        public RetrofitApiBuilder addConvertFactory(Converter.Factory factory) {
             mRetrofitBuilder.addConverterFactory(factory);
             return this;
         }
 
-        public RetrofitAPIBuilder addHttpsSupport() {
-            return this;
-        }
 
         public <T> T  build(Class<T> clazzClientApi) {
 //            OkHttpClient.Builder builder = new OkHttpClient.Builder();
---
+++ retrofit-base/framework/src/main/java/com/linxiao/framework/support/FileOperator.java
@@ -0,0 +1,60 @@
+package com.linxiao.framework.support;
+
+/**
+ * 文件管理封装
+ * 封装成一个针对指定路径的操作对象
+ * Created by LinXiao on 2016-12-13.
+ */
+public class FileOperator {
+
+
+
+    /**
+     * 将文件操作对象指定到某一路径，如果不存在则打开
+     * */
+    public static void openOrCreateDir(String dir) {
+
+    }
+
+    /**
+     * 打开某一路径，不存在则操作失败
+     * */
+    public static void openDir(String dir) {
+
+    }
+
+    /**
+     * 检查文件是否存在
+     * */
+    public static boolean isExist() {
+        return false;
+    }
+
+    /**
+     * 新建文件夹
+     * */
+    public static void mkdir() {
+
+    }
+
+    /**
+     * 新建文件
+     * */
+    public static void mkFile() {
+
+    }
+
+    /**
+     * 重命名文件或文件夹
+     * */
+    public static void rename() {
+
+    }
+
+    /**
+     * 删除文件或文件夹
+     * */
+    public static void delete() {
+
+    }
+}
