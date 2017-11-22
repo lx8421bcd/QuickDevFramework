14f776997786f1eddf37aff97de0d034ba18f745
LinXiao <tsunamibad@qq.com>
2017-11-22 16:01:59 +0800

    StatusBarUtil更新MIUI设置浅色模式方法，添加无法设置机型黑名单

--- framework/src/main/java/com/linxiao/framework/util/StatusBarUtil.java
+++ framework/src/main/java/com/linxiao/framework/util/StatusBarUtil.java
@@ -3,12 +3,16 @@ package com.linxiao.framework.util;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Build;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Android 状态栏工具类
@@ -16,12 +20,18 @@ import java.lang.reflect.Method;
  * Created by linxiao on 2017/7/18.
  */
 public final class StatusBarUtil {
-    
+    public static final String TAG = StatusBarUtil.class.getSimpleName();
+
+    /**
+     * API 23 以上无法由于修改底层实现而无法开启浅色状态栏的ROM黑名单
+     * */
+    private static final List<String> LIGHT_MODE_BLOCK_LIST = Arrays.asList("A Part Of Build.DISPLAY", "ZUI_2.3");
+
     /**
      * 修改状态栏为全透明
      * @param activity 需设置样式的Activity
      */
-    public static void transparencyBar(Activity activity){
+    public static void transparencyBar(Activity activity) {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = activity.getWindow();
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
@@ -37,7 +47,19 @@ public final class StatusBarUtil {
                     WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         }
     }
-    
+
+    /**
+     * 设置状态栏颜色，5.0以上生效
+     * @param activity 需设置样式的activity
+     * @param  color 颜色值
+     * */
+    public static void setStatusBarColor(Activity activity, int color) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = activity.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(color);
+        }
+    }
     
     /**
      * 设置状态栏深浅样式
@@ -52,30 +74,65 @@ public final class StatusBarUtil {
         if (activity == null) {
             return false;
         }
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+        Log.d(TAG, "hardInfo:" + getHandSetInfo());
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
             return false;
-        }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            activity.getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-            return true;
         }
         if (setStatusBarLightModeFlyme(activity.getWindow(), isLight)) {
             return true;
         }
-        // 这里可以添加其他适配
-        return setStatusBarLightModeMIUI(activity.getWindow(), isLight);
+        if (setStatusBarLightModeMIUI(activity.getWindow(), isLight)) {
+            return true;
+        }
+        // 这里可以添加其他适配，由于某些第三方UI使用原生方法不管用，因此建议第三方UI的适配放在原生前
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return false;
+        }
+        // 检查机型是否在无法显示浅色模式的黑名单中
+        for (String blockROMName : LIGHT_MODE_BLOCK_LIST) {
+            if (Build.DISPLAY.contains(blockROMName)) {
+                return false;
+            }
+        }
+        int cachedParams = activity.getWindow().getDecorView().getSystemUiVisibility();
+        if (isLight) {
+            activity.getWindow().getDecorView().setSystemUiVisibility(
+                    cachedParams | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+        else {
+            activity.getWindow().getDecorView().setSystemUiVisibility(
+                    cachedParams & ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+        return true;
     }
-    
+
+    private static String getHandSetInfo() {
+        String handSetInfo = "手机型号:" + Build.MODEL
+                + "\n系统版本:" + Build.VERSION.RELEASE
+                + "\n产品型号:" + Build.PRODUCT
+                + "\n版本显示:" + Build.DISPLAY
+                + "\n系统定制商:" + Build.BRAND
+                + "\n设备参数:" + Build.DEVICE
+                + "\n开发代号:" + Build.VERSION.CODENAME
+                + "\nSDK版本号:" + Build.VERSION.SDK_INT
+                + "\nCPU类型:" + Build.CPU_ABI
+                + "\n硬件类型:" + Build.HARDWARE
+                + "\n主机:" + Build.HOST
+                + "\n生产ID:" + Build.ID
+                + "\nROM制造商:" + Build.MANUFACTURER // 这行返回的是rom定制商的名称
+                ;
+        return handSetInfo;
+    }
+
     /**
      * 设置状态栏图标为深色和魅族特定的文字风格，Flyme4.0以上
      * 可以用来判断是否为Flyme用户
      * @param window 需要设置的窗口
-     * @param dark 是否把状态栏字体及图标颜色设置为深色
+     * @param isLightMode 是否把状态栏字体及图标颜色设置为深色
      * @return  boolean 成功执行返回true
      *
      */
-    private static boolean setStatusBarLightModeFlyme(Window window, boolean dark) {
+    private static boolean setStatusBarLightModeFlyme(Window window, boolean isLightMode) {
         if (window == null) {
             return false;
         }
@@ -90,7 +147,7 @@ public final class StatusBarUtil {
             meizuFlags.setAccessible(true);
             int bit = darkFlag.getInt(null);
             int value = meizuFlags.getInt(lp);
-            if (dark) {
+            if (isLightMode) {
                 value |= bit;
             } else {
                 value &= ~bit;
@@ -98,19 +155,19 @@ public final class StatusBarUtil {
             meizuFlags.setInt(lp, value);
             window.setAttributes(lp);
             result = true;
-        } catch (Exception e) {}
+        } catch (Exception ignored) {}
         return result;
     }
     
     /**
      * 设置状态栏字体图标为深色，需要MIUIV6以上
      * @param window 需要设置的窗口
-     * @param dark 是否把状态栏字体及图标颜色设置为深色
+     * @param isLightMode 是否把状态栏字体及图标颜色设置为深色
      * @return  boolean 成功执行返回true
      *
      */
     @SuppressWarnings("unchecked")
-    private static boolean setStatusBarLightModeMIUI(Window window, boolean dark) {
+    private static boolean setStatusBarLightModeMIUI(Window window, boolean isLightMode) {
         if (window == null) {
             return false;
         }
@@ -122,13 +179,24 @@ public final class StatusBarUtil {
             Field  field = layoutParams.getField("EXTRA_FLAG_STATUS_BAR_DARK_MODE");
             darkModeFlag = field.getInt(layoutParams);
             Method extraFlagField = clazz.getMethod("setExtraFlags", int.class, int.class);
-            if(dark){
+            if(isLightMode){
                 extraFlagField.invoke(window,darkModeFlag,darkModeFlag);//状态栏透明且黑色字体
             }else{
                 extraFlagField.invoke(window, 0, darkModeFlag);//清除黑色字体
             }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                int cachedParams = window.getDecorView().getSystemUiVisibility();
+                if (isLightMode) {
+                    window.getDecorView().setSystemUiVisibility(
+                            cachedParams | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+                }
+                else {
+                    window.getDecorView().setSystemUiVisibility(
+                            cachedParams & ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+                }
+            }
             result=true;
-        }catch (Exception e){}
+        }catch (Exception ignored){}
         return result;
     }
     
