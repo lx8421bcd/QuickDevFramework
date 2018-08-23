f16846afbf836b60e68078093be9d1e5971639c9
linxiao <tsunamibad@qq.com>
2018-08-23 16:15:05 +0800

    等距装饰器添加边界控制，以便更好应对实际微调

--- framework/src/main/java/com/linxiao/framework/list/EquidistantDecoration.java
+++ framework/src/main/java/com/linxiao/framework/list/EquidistantDecoration.java
@@ -7,7 +7,6 @@ import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-
 /**
  * RecyclerView 等距布局装饰器
  * <p>用于实现RecyclerView中各个项目之间拥有相同间隔的布局效果,
@@ -15,7 +14,8 @@ import android.view.View;
  * </p>
  * <strong>此装饰器不适用于瀑布流布局</strong>
  *
- * Created by linxiao on 2017/11/09.
+ * @author linxiao
+ * Create on 2017-11-09
  */
 public class EquidistantDecoration extends RecyclerView.ItemDecoration {
     /**
@@ -26,7 +26,10 @@ public class EquidistantDecoration extends RecyclerView.ItemDecoration {
      * 横向布局
      * */
     public static final int HORIZONTAL = 1;
-
+    /**
+     * 边界控制，使用spacing值
+     */
+    public static final int BORDER_DEFAULT = -1;
     // 布局方向
     private int orientation = VERTICAL;
     // 行/列数
@@ -35,7 +38,11 @@ public class EquidistantDecoration extends RecyclerView.ItemDecoration {
     private int spacingSize;
     // 间距内容
     private Drawable spacingDrawable;
-
+    // 边界宽度
+    private int borderLeftWidth = BORDER_DEFAULT;
+    private int borderRightWidth = BORDER_DEFAULT;
+    private int borderTopWidth = BORDER_DEFAULT;
+    private int borderBottomWidth = BORDER_DEFAULT;
     /**
      * @param spanCount 列/行数
      * @param spacingSize 间隔大小，单位 px
@@ -54,28 +61,40 @@ public class EquidistantDecoration extends RecyclerView.ItemDecoration {
         this.spacingSize = spacingSize;
         spacingDrawable = new ColorDrawable(Color.TRANSPARENT);
     }
-
     public void setSpacingDrawable(Drawable spacingDrawable) {
         if (spacingDrawable == null) {
             return;
         }
         this.spacingDrawable = spacingDrawable;
     }
-
+    public void setBorderWidth(int width) {
+        borderLeftWidth = width;
+        borderRightWidth = width;
+        borderTopWidth = width;
+        borderBottomWidth = width;
+    }
+    public void setBorderLeftWidth(int borderLeftWidth) {
+        this.borderLeftWidth = borderLeftWidth;
+    }
+    public void setBorderRightWidth(int borderRightWidth) {
+        this.borderRightWidth = borderRightWidth;
+    }
+    public void setBorderTopWidth(int borderTopWidth) {
+        this.borderTopWidth = borderTopWidth;
+    }
+    public void setBorderBottomWidth(int borderBottomWidth) {
+        this.borderBottomWidth = borderBottomWidth;
+    }
     @Override
     public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
         int position = parent.getChildLayoutPosition(view);
-
         int[] rect = getItemOffsetRect(position, state.getItemCount());
-
         outRect.set(rect[0], rect[1], rect[2], rect[3]);
     }
-
     @Override
     public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
         super.onDraw(c, parent, state);
         int itemCount = parent.getChildCount();
-
         int left, right, top, bottom;
         for(int i = 0; i < itemCount; i++) {
             View item = parent.getChildAt(i);
@@ -111,96 +130,108 @@ public class EquidistantDecoration extends RecyclerView.ItemDecoration {
             spacingDrawable.draw(c);
         }
     }
-    
     private int[] getItemOffsetRect(int position, int itemCount) {
-        int left, top, right, bottom;
-        //纵向
         if (orientation == VERTICAL) {
-            int lastRowStartIndex = itemCount - spanCount + itemCount % spanCount;
-            // 只有一列
-            if (spanCount == 1) {
-                left = right = spacingSize;
-            }
-            // 纵向第一列
-            else if(position % spanCount == 0) {
-                left = spacingSize;
-                if (spanCount == 2) {
-                    right = (int) (spacingSize * 1.0f / 2);
-                }
-                else {
-                    right = (int) (spacingSize * 1.0f / 3);
-                }
-            }
-            // 纵向最后一列
-            else if ((position + 1) % spanCount == 0) {
-                if (spanCount == 2) {
-                    left = (int) (spacingSize * 1.0f / 2);
-                }
-                else {
-                    left = (int) (spacingSize * 1.0f / 3);
-                }
-                right = spacingSize;
-            }
-            else {
-                left = right = (int) (spacingSize * 2.0f / 3);
-            }
-            // 纵向第一行
-            if(position < spanCount) {
-                top = spacingSize;
-                bottom = (int) (spacingSize * 1.0f / 2);
-            }
-            // 纵向最后一行
-            else if (position >= lastRowStartIndex) {
-                top = (int) (spacingSize * 1.0f / 2);
-                bottom = spacingSize;
-            }
-            else {
-                top = bottom = (int) (spacingSize * 1.0f / 2);
-            }
+            return offsetVerticalOrientation(position, itemCount);
         }
-        // 横向
         else {
-            int lastColumnStartIndex = itemCount - spanCount + itemCount % spanCount;
-            // 只有一行
-            if (spanCount == 1) {
-                top = bottom = spacingSize;
-            }
-            // 横向第一行
-            else if(position % spanCount == 0) {
-                top = spacingSize;
-                if (spanCount == 2) {
-                    bottom = (int) (spacingSize * 1.0f / 2);
-                }
-                else {
-                    bottom = (int) (spacingSize * 1.0f / 3);
-                }
-            }
-            else if((position + 1) % spanCount == 0) {
-                if (spanCount == 2) {
-                    top = (int) (spacingSize * 1.0f / 2);
-                }
-                else {
-                    top = (int) (spacingSize * 1.0f / 3);
-                }
-                bottom = spacingSize;
-            }
-            else {
-                top = bottom = (int) (spacingSize * 2.0f / 3);
-            }
-            
-            // 横向第一列
-            if (position < spanCount) {
-                left = spacingSize;
+            return offsetHorizontalOrientation(position, itemCount);
+        }
+    }
+    /**
+     * 纵向边距计算
+     */
+    private int[] offsetVerticalOrientation(int position, int itemCount) {
+        int left, top, right, bottom;
+        int lastRowStartIndex = itemCount - spanCount + itemCount % spanCount;
+        // 只有一列
+        if (spanCount == 1) {
+            left = borderLeftWidth >= 0 ? borderLeftWidth : spacingSize;
+            right = borderRightWidth >= 0 ? borderRightWidth : spacingSize;
+        }
+        // 纵向第一列
+        else if(position % spanCount == 0) {
+            left = borderLeftWidth >= 0 ? borderLeftWidth : spacingSize;
+            if (spanCount == 2) {
                 right = (int) (spacingSize * 1.0f / 2);
             }
-            // 横向最后一列
-            else if (position >= lastColumnStartIndex) {
+            else {
+                right = (int) (spacingSize * 1.0f / 3);
+            }
+        }
+        // 纵向最后一列
+        else if ((position + 1) % spanCount == 0) {
+            if (spanCount == 2) {
                 left = (int) (spacingSize * 1.0f / 2);
-                right = spacingSize;
             }
             else {
-                left = right = (int) (spacingSize * 1.0f / 2);
+                left = (int) (spacingSize * 1.0f / 3);
             }
+            right = borderRightWidth >= 0 ? borderRightWidth : spacingSize;
+        }
+        else {
+            left = right = (int) (spacingSize * 2.0f / 3);
+        }
+        // 纵向第一行
+        if(position < spanCount) {
+            top = borderTopWidth >= 0 ? borderTopWidth : spacingSize;
+            bottom = (int) (spacingSize * 1.0f / 2);
+        }
+        // 纵向最后一行
+        else if (position >= lastRowStartIndex) {
+            top = (int) (spacingSize * 1.0f / 2);
+            bottom = borderBottomWidth >= 0 ? borderBottomWidth : spacingSize;
+        }
+        else {
+            top = bottom = (int) (spacingSize * 1.0f / 2);
+        }
+        return new int[]{left, top, right, bottom};
+    }
+    /**
+     * 横向边距计算
+     */
+    private int[] offsetHorizontalOrientation(int position, int itemCount) {
+        int left, top, right, bottom;
+        int lastColumnStartIndex = itemCount - spanCount + itemCount % spanCount;
+        // 只有一行
+        if (spanCount == 1) {
+            top = borderTopWidth >= 0 ? borderTopWidth : spacingSize;
+            bottom = borderBottomWidth >= 0 ? borderBottomWidth : spacingSize;
+        }
+        // 横向第一行
+        else if(position % spanCount == 0) {
+            top = borderTopWidth >= 0 ? borderTopWidth : spacingSize;
+            if (spanCount == 2) {
+                bottom = (int) (spacingSize * 1.0f / 2);
+            }
+            else {
+                bottom = (int) (spacingSize * 1.0f / 3);
+            }
+        }
+        else if((position + 1) % spanCount == 0) {
+            if (spanCount == 2) {
+                top = (int) (spacingSize * 1.0f / 2);
+            }
+            else {
+                top = (int) (spacingSize * 1.0f / 3);
+            }
+            bottom = borderBottomWidth >= 0 ? borderBottomWidth : spacingSize;
+        }
+        else {
+            top = bottom = (int) (spacingSize * 2.0f / 3);
+        }
+        // 横向第一列
+        if (position < spanCount) {
+            left = borderLeftWidth >= 0 ? borderLeftWidth : spacingSize;
+            right = (int) (spacingSize * 1.0f / 2);
+        }
+        // 横向最后一列
+        else if (position >= lastColumnStartIndex) {
+            left = (int) (spacingSize * 1.0f / 2);
+            right = borderRightWidth >= 0 ? borderRightWidth : spacingSize;
+        }
+        else {
+            left = right = (int) (spacingSize * 1.0f / 2);
         }
         return new int[]{left, top, right, bottom};
     }
