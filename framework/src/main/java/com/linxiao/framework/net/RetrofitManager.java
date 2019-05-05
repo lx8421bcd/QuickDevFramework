7bbaf66ff1df69b14cbed88cbad23fee23bf03e5
linxiao <tsunamibad@qq.com>
2019-05-05 15:45:47 +0800

    upload SSLHelper, reformat RetrofitManager

--- framework/src/main/java/com/linxiao/framework/net/RetrofitManager.java
+++ framework/src/main/java/com/linxiao/framework/net/RetrofitManager.java
@@ -1,11 +1,8 @@
 package com.linxiao.framework.net;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
-import android.support.annotation.NonNull;
 import android.text.TextUtils;
 
 import com.franmontiel.persistentcookiejar.ClearableCookieJar;
@@ -13,27 +10,14 @@ import com.franmontiel.persistentcookiejar.PersistentCookieJar;
 import com.franmontiel.persistentcookiejar.cache.SetCookieCache;
 import com.franmontiel.persistentcookiejar.persistence.SharedPrefsCookiePersistor;
 import com.linxiao.framework.common.GlobalContext;
-import com.linxiao.framework.log.Logger;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.math.BigInteger;
-import java.security.KeyManagementException;
+import java.io.File;
 import java.security.KeyStore;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.security.cert.CertificateException;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
-import java.security.interfaces.RSAPublicKey;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
@@ -42,10 +26,8 @@ import javax.net.ssl.X509TrustManager;
 import okhttp3.Cookie;
 import okhttp3.CookieJar;
 import okhttp3.HttpUrl;
-import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 
@@ -62,7 +44,7 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 public class RetrofitManager {
 
     private static final String TAG = RetrofitManager.class.getSimpleName();
-    
+
     private static ClearableCookieJar cookieJar;
     private static HttpInfoCatchInterceptor infoCatchInterceptor;
     private static OkHttpClient mOkHttpClient;
@@ -194,7 +176,7 @@ public class RetrofitManager {
         
         // Https trust config, you can use trust all
         // in debug mode to catch http info more easier
-        builder = configTrustAll(builder);
+        configTrustAll(builder);
         
         // if you want to do some custom header modification before request and effect on global,
         // you should do it at here
@@ -232,23 +214,8 @@ public class RetrofitManager {
     /**
      * https TrustAll 配置，如果需要抓包的话可以打开此配置，否则没有必要
      * */
-    public static OkHttpClient.Builder configTrustAll(OkHttpClient.Builder builder) {
-        final TrustManager[] trustAllCerts = new TrustManager[] {new X509TrustManager() {
-            @SuppressLint("TrustAllX509TrustManager")
-            @Override
-            public void checkClientTrusted(X509Certificate[] chain, String authType)
-                    throws CertificateException {}
-            
-            @SuppressLint("TrustAllX509TrustManager")
-            @Override
-            public void checkServerTrusted(X509Certificate[] chain, String authType)
-                    throws CertificateException {}
-            
-            @Override
-            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                return new java.security.cert.X509Certificate[]{};
-            }
-        }};
+    public static void configTrustAll(OkHttpClient.Builder builder) {
+        final TrustManager[] trustAllCerts = new TrustManager[] {SSLHelper.createTrustAllTrustManager()};
         try {
             // Install the all-trusting trust manager
             final SSLContext sslContext = SSLContext.getInstance("SSL");
@@ -260,117 +227,25 @@ public class RetrofitManager {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return builder;
     }
-    
+
     /**
-     * 信任配置
-     * <p>本地不存放证书时使用</p>
-     * */
-    public static OkHttpClient.Builder configTrust(OkHttpClient.Builder builder, String publicKey) {
-        X509TrustManager trustManager = getDefaultTrustManager(publicKey);
-        final TrustManager[] trustAllCerts = new TrustManager[]{trustManager};
-        // Install the all-trusting trust manager
-        final SSLContext sslContext;
+     * SSL single certificate config for OkHttpClient
+     */
+    public static void configSingleTrust(OkHttpClient.Builder builder) {
         try {
-            sslContext = SSLContext.getInstance("TLS");
-            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
-        
-        } catch (NoSuchAlgorithmException | KeyManagementException e) {
-            Logger.e(TAG, e);
-            return builder;
-        }
-        // Create an ssl socket factory with our all-trusting manager
-        SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
-        builder.sslSocketFactory(sslSocketFactory, (X509TrustManager)trustAllCerts[0]);
-        return builder;
-    }
-    
-    protected static HostnameVerifier getHostnameVerifier(final String[] hostUrls) {
-        return (hostname, session) -> {
-            boolean ret = false;
-            for (String host : hostUrls) {
-                if (host.equalsIgnoreCase(hostname)) {
-                    ret = true;
-                }
-            }
-            return ret;
-        };
-    }
-    
-    private static X509TrustManager getDefaultTrustManager(final String publicKey) {
-        return new X509TrustManager() {
-            @Override
-            public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-                if (chain == null) {
-                    throw new IllegalArgumentException("checkServerTrusted:x509Certificate array is null");
-                }
-                if (chain.length <= 0) {
-                    throw new IllegalArgumentException("checkServerTrusted: X509Certificate is empty");
-                }
-            }
-
-            @Override
-            public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-                if (!(null != authType && authType.equalsIgnoreCase("RSA"))) {
-                    throw new CertificateException("checkServerTrusted: AuthType is not RSA");
-                }
-                // Perform customary SSL/TLS checks
-                try {
-                    TrustManagerFactory tmf = TrustManagerFactory.getInstance("X509");
-                    tmf.init((KeyStore) null);
-                    for (TrustManager trustManager : tmf.getTrustManagers()) {
-                        ((X509TrustManager) trustManager).checkServerTrusted(chain, authType);
-                    }
-                } catch (Exception e) {
-                    throw new CertificateException(e);
-                }
-                // Hack ahead: BigInteger and toString(). We know a DER encoded Public Key begins
-                // with 0×30 (ASN.1 SEQUENCE and CONSTRUCTED), so there is no leading 0×00 to drop.
-                RSAPublicKey pubKey = (RSAPublicKey) chain[0].getPublicKey();
-                String encoded = new BigInteger(1 /* positive */, pubKey.getEncoded()).toString(16);
-                // Pin it!
-                final boolean expected = publicKey.equalsIgnoreCase(encoded);
-
-                if (!expected) {
-                    throw new CertificateException("checkServerTrusted: Expected public key: "
-                            + publicKey + ", got public key:" + encoded);
-                }
-            }
-
-            @Override
-            public X509Certificate[] getAcceptedIssuers() {
-                return new X509Certificate[0];
-            }
-        };
-    }
-
-    private static SSLSocketFactory getSSLSocketFactory(@NonNull Context context, int[] certificates) {
-        //CertificateFactory用来证书生成
-        CertificateFactory certificateFactory;
-        try {
-            certificateFactory = CertificateFactory.getInstance("X.509");
-            //Create a KeyStore containing our trusted CAs
-            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-            keyStore.load(null, null);
-
-            for (int i = 0; i < certificates.length; i++) {
-                //读取本地证书
-                InputStream is = context.getResources().openRawResource(certificates[i]);
-                keyStore.setCertificateEntry(String.valueOf(i), certificateFactory.generateCertificate(is));
-                is.close();
-            }
-            //Create a TrustManager that trusts the CAs in our keyStore
-            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-            trustManagerFactory.init(keyStore);
-            //Create an SSLContext that uses our TrustManager
-            SSLContext sslContext = SSLContext.getInstance("TLS");
-            sslContext.init(null, trustManagerFactory.getTrustManagers(), new SecureRandom());
-            return sslContext.getSocketFactory();
+            //this file do not exist, replace with your certificate file when you use this method
+            KeyStore keyStore = SSLHelper.createKeyStore(new File("test.bks"), "123456");
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance("X509");
+            tmf.init(keyStore);
+            TrustManager[] trustManagers = tmf.getTrustManagers();
+            SSLContext sslContext = SSLContext.getInstance("SSL");
+            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+            builder.sslSocketFactory(sslSocketFactory, (X509TrustManager) trustManagers[0]);
+            builder.hostnameVerifier((hostname, session) -> true);
         } catch (Exception e) {
-            Logger.e(TAG, e);
+            e.printStackTrace();
         }
-        return null;
-    }
 
+    }
 }
---
+++ framework/src/main/java/com/linxiao/framework/net/SSLHelper.java
@@ -0,0 +1,176 @@
+package com.linxiao.framework.net;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.annotation.RawRes;
+
+import com.linxiao.framework.common.ContextProvider;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+/**
+ * tool class for user to build SSL configs for TCP connect
+ * <p>
+ * details for class usage and attention
+ * </p>
+ *
+ * @author linxiao
+ * @since 2019-04-28
+ */
+public final class SSLHelper {
+
+
+    /**
+     * get public key from certificate file in raw folder
+     * @param certificateRes resId of certificate
+     * @return instance of {@link PublicKey}
+     */
+    public static PublicKey getPublicKey(Context context, @RawRes int certificateRes) {
+        try {
+            InputStream fin = context.getResources().openRawResource(certificateRes);
+            CertificateFactory f = CertificateFactory.getInstance("X.509");
+            X509Certificate certificate = (X509Certificate)f.generateCertificate(fin);
+            return certificate.getPublicKey();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * get public key from local certificate file
+     * @param certificatePath file path of certificate
+     * @return instance of {@link PublicKey}
+     */
+    public static PublicKey getPublicKey(String certificatePath) {
+        try {
+            FileInputStream fin = new FileInputStream(certificatePath);
+            CertificateFactory f = CertificateFactory.getInstance("X.509");
+            X509Certificate certificate = (X509Certificate)f.generateCertificate(fin);
+            return certificate.getPublicKey();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * generate a instance of {@link X509TrustManager} config as trust all
+     * @return instance of X509TrustManager
+     */
+    public static TrustManager createTrustAllTrustManager() {
+        return new X509TrustManager() {
+            public X509Certificate[] getAcceptedIssuers() {
+                return new java.security.cert.X509Certificate[]{};
+            }
+            @SuppressLint("TrustAllX509TrustManager")
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                // do nothing
+            }
+            @SuppressLint("TrustAllX509TrustManager")
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                // do nothing
+            }
+        };
+    }
+
+    /**
+     * generate a https HostNameVerifier with inputted urls
+     * @param hostUrls accept host urls
+     */
+    public static HostnameVerifier getHostnameVerifier(final String[] hostUrls) {
+        return (hostname, session) -> {
+            boolean ret = false;
+            for (String host : hostUrls) {
+                if (host.equalsIgnoreCase(hostname)) {
+                    ret = true;
+                }
+            }
+            return ret;
+        };
+    }
+
+    /**
+     * generate a https HostNameVerifier accept all host
+     */
+    public static HostnameVerifier getTrustAllVerifier() {
+        return (hostname, session) -> true;
+    }
+
+
+    /**
+     * generate a {@link KeyStore} Object from KeyStore file
+     * <p>
+     * attention: Android only support BKS format key store, which means you have to use
+     * ".bks" certificate file format, you have to convert other key store format to bks
+     * to use it in Android
+     * </p>
+     *
+     * @param keyStoreFileStream key store file input stream from local file, such as raw resource and sdcard
+     * @param password key store password
+     * @return {@link KeyStore} instance
+     */
+    public static KeyStore createKeyStore(InputStream keyStoreFileStream, String password) throws
+            KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException {
+        if (keyStoreFileStream == null) {
+            return null;
+        }
+        KeyStore keyStore = KeyStore.getInstance("BKS");
+        keyStore.load(keyStoreFileStream, password.toCharArray());
+        return keyStore;
+    }
+
+    /**
+     * generate a {@link KeyStore} Object from KeyStore file
+     * <p>
+     * attention: Android only support BKS format key store, which means you have to use
+     * ".bks" certificate file format, you have to convert other key store format to bks
+     * to use it in Android
+     * </p>
+     *
+     * @param rawResId resId of certificate file in raw resource folder
+     * @param password key store password
+     * @return {@link KeyStore} instance
+     */
+    public static KeyStore createKeyStore(@RawRes int rawResId, String password)
+            throws CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException {
+        InputStream is = ContextProvider.get().getResources().openRawResource(rawResId);
+        return createKeyStore(is, password);
+    }
+
+    /**
+     * generate a {@link KeyStore} Object from KeyStore file
+     * <p>
+     * attention: Android only support BKS format key store, which means you have to use
+     * ".bks" certificate file format, you have to convert other key store format to bks
+     * to use it in Android
+     * </p>
+     *
+     * @param certFile certificate file object
+     * @param password key store password
+     * @return {@link KeyStore} instance
+     */
+    public static KeyStore createKeyStore(File certFile, String password)
+            throws CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException {
+        InputStream is = new FileInputStream(certFile);
+        return createKeyStore(is, password);
+    }
+
+}
