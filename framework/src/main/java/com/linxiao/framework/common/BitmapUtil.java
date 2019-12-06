338a1becc1724b0e9e8536b5e7ab021d53f8c966
linxiao <tsunamibad@qq.com>
2019-12-06 16:39:08 +0800

    commit simple image upload tool

--- framework/src/main/java/com/linxiao/framework/common/BitmapUtil.java
+++ framework/src/main/java/com/linxiao/framework/common/BitmapUtil.java
@@ -1,10 +1,33 @@
 package com.linxiao.framework.common;
 
+import android.content.ContentResolver;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.NinePatch;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.NinePatchDrawable;
+import android.net.Uri;
 import android.os.Build;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import static android.os.Build.VERSION_CODES.KITKAT;
 
@@ -14,7 +37,197 @@ import static android.os.Build.VERSION_CODES.KITKAT;
  * Created by linxiao on 2016/7/30.
  */
 public class BitmapUtil {
-    
+
+
+    public static Bitmap getBitmap(String filePath) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(filePath, options);
+    }
+
+    /**
+     * 通过URI加载图片
+     *
+     * @param context
+     * @param uri     图片URI
+     * @return
+     * @throws IOException
+     */
+    public static Bitmap getBitmap(Context context, Uri uri) throws IOException {
+        return getScaledBitmap(context, uri, Bitmap.Config.ARGB_8888, Integer.MAX_VALUE, Integer.MAX_VALUE);
+    }
+
+    /**
+     * 通过URI加载图片
+     *
+     * @param context
+     * @param uri     图片URI
+     * @param config
+     * @return
+     * @throws IOException
+     */
+    public static Bitmap getScaledBitmap(Context context, Uri uri, Bitmap.Config config, int maxWidth, int maxHeight) throws IOException {
+        maxWidth = maxWidth <= 0 ? Integer.MAX_VALUE : maxWidth;
+        maxHeight = maxHeight <= 0 ? Integer.MAX_VALUE : maxHeight;
+        ContentResolver cr = context.getContentResolver();
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        opts.inJustDecodeBounds = true; // 设置为true, 加载器不会返回图片,
+        // 而是设置Options对象中以out开头的字段.即仅仅解码边缘区域
+        InputStream is = cr.openInputStream(uri);
+        BitmapFactory.decodeStream(is, null, opts);
+        // 得到图片的宽和高
+        int imageWidth = opts.outWidth;
+        int imageHeight = opts.outHeight;
+        // 计算缩放比例
+        int scale = 1;
+        if (imageWidth > maxWidth || imageHeight > maxHeight) {
+            int widthScale = imageWidth / maxWidth;
+            int heightScale = imageHeight / maxHeight;
+            scale = Math.max(widthScale, heightScale);
+        }
+        // 指定加载可以加载出图片.
+        opts.inJustDecodeBounds = false;
+        // 使用计算出来的比例进行缩放(这里的缩放只会以2的幂次方缩放)
+        opts.inSampleSize = (int) Math.floor(Math.log(scale) / Math.log(2));
+        opts.inPreferredConfig = config;
+        is = cr.openInputStream(uri);
+        Bitmap ret = BitmapFactory.decodeStream(is, null, opts);
+        if (ret == null) {
+            return null;
+        }
+        // 以上的绽放不精确,以下缩放到指定的大小
+        imageWidth = ret.getWidth();
+        imageHeight = ret.getHeight();
+        if (imageWidth > maxWidth || imageHeight > maxHeight) {
+            float widthScale = 1.0F * maxWidth / imageWidth;
+            float heightScale = 1.0F * maxHeight / imageHeight;
+            float fScale = Math.min(widthScale, heightScale);
+            int dstWidth = (int) (imageWidth * fScale);
+            int dstHeight = (int) (imageHeight * fScale);
+            return Bitmap.createScaledBitmap(ret, dstWidth, dstHeight, false);
+        }
+        return ret;
+    }
+
+
+    public static BitmapFactory.Options getBitmapOptions(String filePath) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;// 不去真的解析图片，只是获取图片的头部信息，包含宽高等；
+        BitmapFactory.decodeFile(filePath, options);
+        return options;
+    }
+
+    /**
+     * bitmap转为base64
+     *
+     * @param bitmap
+     * @return
+     * @throws IOException
+     */
+    public static String bitmapToBase64(Bitmap bitmap) throws IOException {
+        if (bitmap == null) {
+            return null;
+        }
+        String result = null;
+        ByteArrayOutputStream baos = null;
+        baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, baos);
+        baos.flush();
+        baos.close();
+        byte[] bitmapBytes = baos.toByteArray();
+        result = Base64.encodeToString(bitmapBytes, Base64.DEFAULT);
+        return result;
+    }
+
+    /**
+     * base64转为bitmap
+     *
+     * @param base64Data
+     * @return
+     */
+    public static Bitmap base64ToBitmap(String base64Data) {
+        byte[] bytes = Base64.decode(base64Data, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+    }
+
+    public static Bitmap getbitmap(String imageUri) {
+        // 显示网络上的图片
+        Bitmap bitmap = null;
+        try {
+            URL myFileUrl = new URL(imageUri);
+            HttpURLConnection conn = (HttpURLConnection) myFileUrl
+                    .openConnection();
+            conn.setDoInput(true);
+            conn.connect();
+            InputStream is = conn.getInputStream();
+            bitmap = BitmapFactory.decodeStream(is);
+            is.close();
+        } catch (OutOfMemoryError e) {
+            e.printStackTrace();
+            bitmap = null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            bitmap = null;
+        }
+        return bitmap;
+    }
+
+    /**
+     * 截取View的bitmap快照
+     * */
+    public static Bitmap convertViewToBitmap(View view) {
+        if (view == null) {
+            return null;
+        }
+        view.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
+        view.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+        view.buildDrawingCache();
+        return view.getDrawingCache();
+    }
+
+    /**
+     * 创建圆形头像
+     *
+     * @param img
+     * @return
+     */
+    public static Bitmap createRoundIcon(Bitmap img) {
+        if (img == null) {
+            return null;
+        }
+        // 原图大小
+        int imgWidth = img.getWidth();
+        int imgHeight = img.getHeight();
+        // 生成圆形图像大小
+        int imgSize = Math.min(imgWidth, imgHeight);
+        // 绘制图片的起始位置
+        float left = (imgSize - imgWidth) / 2.0F;
+        float right = (imgSize - imgHeight) / 2.0F;
+        // 填充边距
+        int padding = 2;
+        // 图像半径
+        int r = imgSize / 2;
+
+        // 1.创建新图
+        Bitmap ret = Bitmap.createBitmap(imgSize, imgSize, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(ret);
+        Paint p = new Paint();
+        p.setAntiAlias(true);
+
+        // 2.绘制圆形头像
+        p.setColor(Color.WHITE);
+        c.drawCircle(r, r, r - padding, p);
+        p.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        c.drawBitmap(img, left, right, p);
+
+        // 3.绘制边框
+        p.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_OVER));
+        c.drawCircle(r, r, r, p);
+
+        return ret;
+    }
+
     /**
      * 根据设定尺寸压缩图片
      * */
@@ -22,11 +235,11 @@ public class BitmapUtil {
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = true;// 不去真的解析图片，只是获取图片的头部信息，包含宽高等；
         BitmapFactory.decodeFile(pathName, options);
-        
+
         // 得到图片的宽度、高度；
         float imgWidth = options.outWidth;
         float imgHeight = options.outHeight;
-        
+
         int compressW = targetWidth;
         int compressH = targetHeight;
         // 处理横竖问题
@@ -38,58 +251,152 @@ public class BitmapUtil {
             compressW = targetHeight;
             compressH = targetWidth;
         }
-        
+
         // 分别计算图片宽度、高度与目标宽度、高度的比例；取大于等于该比例的最小整数；
         int widthRatio = (int) Math.ceil(imgWidth / (float) compressW);
         int heightRatio = (int) Math.ceil(imgHeight / (float) compressH);
         options.inSampleSize = 1;
-        
+
         // 如果尺寸接近则不压缩，否则进行比例压缩
-        if (widthRatio > 1 || heightRatio > 1) {
+        if (widthRatio > 1 || widthRatio > 1) {
             if (widthRatio > heightRatio) {
                 options.inSampleSize = widthRatio;
             } else {
                 options.inSampleSize = heightRatio;
             }
         }
-        
+
         //设置好缩放比例后，加载图片进内容；
         options.inJustDecodeBounds = false;
         return BitmapFactory.decodeFile(pathName, options);
     }
-    
+
+    /**
+     * 设定限制大小采用缩放法压缩图片，单位 B
+     * <p>每次压缩，等比例缩小图片直到bitmap小于限制大小</p>
+     * */
+    public static Bitmap matrixCompressByLimit(Bitmap bitmap, int quality, int limitSize) {
+        if (bitmap == null) {
+            return null;
+        }
+        while(getBitmapSize(bitmap, quality) > limitSize) {
+            Matrix matrix = new Matrix();
+            matrix.setScale(0.9f, 0.9f);
+            bitmap = Bitmap.createBitmap(bitmap, 0, 0,
+                    bitmap.getWidth(),
+                    bitmap.getHeight(),
+                    matrix,
+                    true);
+            Log.d("matrixCompressByLimit",
+                    " compressedSize: " + getBitmapSize(bitmap) +
+                            " compressedW = " + bitmap.getWidth() +
+                            " compressedH = " + bitmap.getHeight()
+            );
+        };
+        return bitmap;
+    }
+
     /**
      * 设定限制大小压缩图片，单位 B
-     * <p>此方法主要降低的是图像的内存占用，并不会降低图像的存储占用</p>
-     * <p>此方法将会降低图像质量</p>
+     * <p>注意，此方法只压缩了图片在磁盘上的存储空间，直接输出的bitmap大小不会变化</p>
      * */
-    public static Bitmap compressByLimit(Bitmap bitmap, int limitSize) {
+    public static Bitmap compressDiskSizeByLimit(Bitmap bitmap, int limitSize) {
         if (bitmap == null) {
             return null;
         }
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         int options = 100;
-        bitmap.compress(Bitmap.CompressFormat.JPEG, options, baos);
-        while (baos.size() > limitSize && options > 0) {
-            options--;
+        do {
             baos.reset();
             bitmap.compress(Bitmap.CompressFormat.JPEG, options, baos);
-        }
+            Log.d("compressDiskSizeByLimit", "options: " + options);
+            options--;
+            Log.d("compressDiskSizeByLimit", "baos.size : " + baos.toByteArray().length);
+        } while (baos.toByteArray().length > limitSize && options > 0);
+
         byte[] compressedData = baos.toByteArray();
+        try {
+            baos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return BitmapFactory.decodeByteArray(compressedData, 0, compressedData.length);
     }
-    
-    /**
-     * 获取Bitmap 大小，单位 B
-     * */
-    public static int getBitmapSize(Bitmap bitmap) {
-        
-        // From KitKat onward use getAllocationByteCount() as allocated bytes can potentially be
-        // larger than bitmap byte count.
-        if (Build.VERSION.SDK_INT >= KITKAT) {
-            return bitmap.getAllocationByteCount();
+
+    public static long getBitmapSize(Bitmap bitmap) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 80, stream);
+        byte[] imageInByte = stream.toByteArray();
+        long len = imageInByte.length;
+        try {
+            stream.flush();
+            stream.close();
+        } catch (IOException ignored) {}
+        return len;
+    }
+
+    public static long getBitmapSize(Bitmap bitmap, int quality) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, quality, stream);
+        byte[] imageInByte = stream.toByteArray();
+        long len = imageInByte.length;
+        try {
+            stream.flush();
+            stream.close();
+        } catch (IOException ignored) {}
+        return len;
+    }
+
+    public static int getCompressQuality(Bitmap bitmap, long fileSize) {
+        int quality = 100;
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, quality, stream);
+        while(stream.toByteArray().length > fileSize && quality > 10) {
+            stream.reset();
+            quality -= 2;
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, stream);
+            Log.d("ImageUtil", "quality: " + quality);
         }
-        // pre kitkat
-        return bitmap.getByteCount();
+        try {
+            stream.flush();
+            stream.close();
+        } catch (IOException ignored) {}
+        return quality;
+    }
+
+    /**
+     * 加载图片到View
+     *
+     * @param url  图片url
+     * @param view View
+     */
+    public static void loadUrlImageInto(Context context, String url, View view) {
+        try {
+            Bitmap bitmap;
+            if(url.startsWith("file:///android_asset/")) {
+                bitmap = BitmapFactory.decodeStream(
+                        context.getAssets().open(url.substring("file:///android_asset/".length())));
+            } else if (url.startsWith("file://")) {
+                bitmap = BitmapFactory.decodeFile(url.substring("file://".length()));
+            } else {
+                bitmap = BitmapFactory.decodeStream(
+                        context.getContentResolver().openInputStream(Uri.parse(url)));
+            }
+            byte[] chunk = bitmap.getNinePatchChunk();
+            boolean result = NinePatch.isNinePatchChunk(chunk);
+            Drawable drawable;
+            if (result) {
+                drawable = new NinePatchDrawable(bitmap, chunk, new Rect(), null);
+            } else {
+                drawable = new BitmapDrawable(bitmap);
+            }
+            if (view instanceof ImageView) {
+                ((ImageView) view).setImageDrawable(drawable);
+            } else {
+                view.setBackgroundDrawable(drawable);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ framework/src/main/java/com/linxiao/framework/net/SimpleImageUploader.java
@@ -0,0 +1,253 @@
+package com.linxiao.framework.net;
+
+import android.graphics.Bitmap;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.linxiao.framework.common.BitmapUtil;
+import com.linxiao.framework.common.ContextProvider;
+import com.linxiao.framework.permission.PermissionException;
+import com.linxiao.framework.permission.PermissionManager;
+
+import org.json.JSONObject;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.net.URL;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.X509TrustManager;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.schedulers.Schedulers;
+
+/**
+ * simple image upload tool
+ *
+ * <p>
+ * used to handle some simple image upload demand such like upload avatar.
+ * default upload limit size is 10MB, you can change this attribute by using
+ * {@link #setUploadLimitSize(int)}.
+ * default User-Agent is application package name, use {@link #setUserAgent(String)}
+ * to change it globally
+ *
+ * general procedure of image upload is set image local path and remote upload url,
+ * execute upload program and get upload result, which contains uploaded image url.
+ * so the onNext method of Subscriber will returns a JSONObject that convert by
+ * http response body, considering that the response format is determined by the backend
+ * and is actually unknown, this tool do not provide more result parsing
+ *
+ * </p>
+ *
+ * @author linxiao
+ * Create on 2019-12-04
+ */
+public class SimpleImageUploader {
+    
+    private static final String TAG = SimpleImageUploader.class.getSimpleName();
+    
+    private static final int DEFAULT_UPLOAD_LIMIT_SIZE = 10485760;  // default limit 10MB
+    private static final String DEFAULT_USER_AGENT = ContextProvider.get().getPackageName();
+
+    private static String mUserAgent = DEFAULT_USER_AGENT;
+    private String filePath;
+    private String uploadUrl;
+    private String method = "POST";
+    private String cookieString = null;
+    private Map<String, Object> requestParams = new HashMap<>();
+    private int limitSize = DEFAULT_UPLOAD_LIMIT_SIZE;
+
+    private SimpleImageUploader(String filePath, String uploadUrl) {
+        this.filePath = filePath;
+        this.uploadUrl = uploadUrl;
+    }
+
+    /**
+     * set User-Agent string
+     */
+    public static void setUserAgent(String userAgent) {
+        mUserAgent = userAgent;
+    }
+
+
+    public static SimpleImageUploader newInstance(String filePath, String uploadUrl){
+        return new SimpleImageUploader(filePath, uploadUrl);
+    }
+
+    /**
+     * set upload limit size, image will be compressed that over this limit
+     * @param limitSize limit size
+     * */
+    public SimpleImageUploader setUploadLimitSize(int limitSize) {
+        this.limitSize = limitSize;
+        return this;
+    }
+
+    /**
+     * set upload request method, usually POST or GET
+     * using POST by default
+     * @param method method name
+     * */
+    public SimpleImageUploader setMethod(String method) {
+        this.method = method;
+        return this;
+    }
+
+    /**
+     * add additional request params
+     * @param key key
+     * @param value value
+     */
+    public SimpleImageUploader putRequestParams(String key, Object value) {
+        requestParams.put(key, value);
+        return this;
+    }
+
+    public SimpleImageUploader setCookieString(String cookieString) {
+        this.cookieString = cookieString;
+        return this;
+    }
+
+    public String getCookieString() {
+        return cookieString;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public String getUploadUrl() {
+        return uploadUrl;
+    }
+
+    public Observable<JSONObject> start() {
+        if (!PermissionManager.hasSDCardPermission()) {
+            return Observable.error(new PermissionException());
+        }
+        return Observable.create((ObservableOnSubscribe<JSONObject>) emitter -> {
+            String BOUNDARY = UUID.randomUUID().toString();
+            String PREFIX = "--", LINE_END = "\r\n";
+            String CONTENT_TYPE = "multipart/form-data";
+
+            File uploadFile = new File(filePath);
+            if (!uploadFile.exists()) {
+                emitter.onError(new FileNotFoundException());
+                return;
+            }
+            if (!uploadFile.isFile()) {
+                emitter.onError(new FileNotFoundException("target is not a file"));
+                return;
+            }
+            String fileName = uploadFile.getName();
+            Log.d(TAG, "filePath = " + filePath);
+            Log.d(TAG, "fileName = " + fileName);
+            Bitmap uploadImg = BitmapUtil.getBitmap(filePath);
+            int quality = BitmapUtil.getCompressQuality(uploadImg, uploadFile.length());
+            Log.d(TAG, "quality: " + quality);
+            Log.d(TAG, "limitSize: " + limitSize);
+            uploadImg = BitmapUtil.matrixCompressByLimit(uploadImg, quality, limitSize);
+            try {
+                Log.d(TAG, "upload url: " + uploadUrl);
+                URL url = new URL(uploadUrl);
+                trustEveryone();
+                HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+                connection.setReadTimeout(10000);
+                connection.setConnectTimeout(10000);
+                connection.setDoInput(true);
+                connection.setDoOutput(true);
+                connection.setUseCaches(false);
+                connection.setRequestMethod(method);
+                connection.setRequestProperty("User-Agent", mUserAgent);
+                connection.setRequestProperty("Charset", "UTF-8");
+                connection.setRequestProperty("Connection", "keep-alive");
+                connection.setRequestProperty("Content-Type", CONTENT_TYPE + ";boundary=" + BOUNDARY);
+                if (!TextUtils.isEmpty(cookieString)) {
+                    connection.setRequestProperty("Cookie", cookieString);
+                }
+                DataOutputStream dos = new DataOutputStream(connection.getOutputStream());
+                for (String key : requestParams.keySet()) {
+                    dos.writeBytes(PREFIX + BOUNDARY);
+                    dos.writeBytes(LINE_END);
+                    dos.writeBytes("Content-Disposition: form-data; name=\"" + key + "\"" + LINE_END);
+                    dos.writeBytes("Content-Type: text/plain" + LINE_END);
+                    dos.writeBytes(LINE_END);
+                    dos.writeBytes(String.valueOf(requestParams.get(key)));
+                    dos.writeBytes(LINE_END);
+                }
+                dos.writeBytes(PREFIX + BOUNDARY);
+                dos.writeBytes(LINE_END);
+                dos.writeBytes("Content-Disposition: form-data; name=\"file\"; filename=\"" + fileName + "\"" + LINE_END);
+                dos.writeBytes("Content-Type: image/jpeg; charset=" + "UTF-8" + LINE_END);
+                dos.writeBytes(LINE_END);
+
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                uploadImg.compress(Bitmap.CompressFormat.JPEG, quality, baos);
+                InputStream is =  new ByteArrayInputStream(baos.toByteArray());
+                byte[] bytes = new byte[1024];
+                int len;
+                while ((len = is.read(bytes)) != -1) {
+                    dos.write(bytes, 0, len);
+                }
+                is.close();
+                dos.writeBytes(LINE_END);
+                dos.writeBytes(PREFIX + BOUNDARY + PREFIX + LINE_END);
+                dos.flush();
+                dos.close();
+                int code = connection.getResponseCode();
+                Log.d(TAG, "code: " + code);
+                if (code != 200) {
+                    emitter.onError(new ApiException(code, ""));
+                    return;
+                }
+                InputStream input = connection.getInputStream();
+                StringBuilder sb1 = new StringBuilder();
+                int ss;
+                while ((ss = input.read()) != -1) {
+                    sb1.append((char) ss);
+                }
+                String result = sb1.toString();
+                Log.d(TAG, "result: " + result);
+                emitter.onNext(new JSONObject(result));
+                emitter.onComplete();
+            } catch (Exception e) {
+                emitter.onError(e);
+            }
+        })
+        .subscribeOn(Schedulers.io());
+    }
+
+    private void trustEveryone() {
+        try {
+            HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
+            SSLContext context = SSLContext.getInstance("TLS");
+            context.init(null, new X509TrustManager[]{new X509TrustManager() {
+                public void checkClientTrusted(X509Certificate[] chain,
+                                               String authType) {
+                }
+
+                public void checkServerTrusted(X509Certificate[] chain,
+                                               String authType) {
+                }
+
+                public X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[0];
+                }
+            }}, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(
+                    context.getSocketFactory());
+        } catch (Exception e) { // should never happen
+            e.printStackTrace();
+        }
+    }
+}
