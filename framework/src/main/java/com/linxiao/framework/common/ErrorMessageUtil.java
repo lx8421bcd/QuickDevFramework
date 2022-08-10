f3a47654d050f86bcc0d611846c6242a7ca41325
lx8421bcd <lx8421bcd@github.com>
2022-08-10 21:43:11 +0800

    update ErrorMessageUtil

--- framework/src/main/java/com/linxiao/framework/common/ErrorMessageUtil.java
+++ framework/src/main/java/com/linxiao/framework/common/ErrorMessageUtil.java
@@ -1,27 +1,43 @@
 package com.linxiao.framework.common;
 
-import com.linxiao.framework.net.ApiException;
 import com.linxiao.framework.permission.PermissionException;
 
 import java.net.UnknownHostException;
+import java.util.HashMap;
 
 import retrofit2.HttpException;
 
 public class ErrorMessageUtil {
 
-    protected String getMessageString(Throwable e) {
-        if (e instanceof ApiException) {
-            return e.getMessage();
-        }
-        if (e instanceof UnknownHostException) {
+    public interface ErrorMessageParser {
+        String parseMessage(Throwable e);
+    }
+
+    private static final HashMap<Class<? extends Throwable>, ErrorMessageParser> errorMessageMap = new HashMap<>();
+
+    static {
+        errorMessageMap.put(UnknownHostException.class, e -> {
             return "unknown host";
-        }
-        if (e instanceof HttpException) {
-            return "http error(" + ((HttpException) e).code() + ")";
-        }
-        if (e instanceof PermissionException) {
+        });
+        errorMessageMap.put(HttpException.class, e -> {
+            return "http error(" + ((HttpException)e).code() + ")";
+        });
+        errorMessageMap.put(PermissionException.class, e -> {
             return "permission denied";
+        });
+    }
+    public static String getMessageString(Throwable e) {
+        ErrorMessageParser parser = errorMessageMap.get(e.getClass());
+        if (parser != null) {
+            return parser.parseMessage(e);
         }
         return e.getMessage();
+    }
+
+    private static void setErrorMessageParser(Class<? extends Throwable> clazz, ErrorMessageParser parser) {
+        if (parser == null) {
+            return;
+        }
+        errorMessageMap.put(clazz, parser);
     }
 }
