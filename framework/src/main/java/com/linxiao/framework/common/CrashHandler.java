ead0524261447684a24aeb248ba8c340cb7ccbb2
lx8421bcd <lx8421bcd@github.com>
2022-08-10 17:06:37 +0800

    update CrashHandler.java

--- framework/src/main/java/com/linxiao/framework/common/CrashHandler.java
+++ framework/src/main/java/com/linxiao/framework/common/CrashHandler.java
@@ -3,25 +3,35 @@ package com.linxiao.framework.common;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Build;
-import android.os.Looper;
-import android.os.SystemClock;
+import android.text.TextUtils;
 import android.util.Log;
-import android.widget.Toast;
 
-import com.linxiao.framework.R;
-import com.linxiao.framework.file.FileUtil;
+import androidx.annotation.NonNull;
+import androidx.collection.ArrayMap;
 
-import java.io.File;
-import java.io.IOException;
+import com.google.gson.reflect.TypeToken;
+import com.linxiao.framework.net.RetrofitManager;
+import com.linxiao.framework.preferences.AppPreferences;
+import com.linxiao.framework.rx.RxSubscriber;
+
+import org.json.JSONObject;
+
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.lang.Thread.UncaughtExceptionHandler;
 import java.lang.reflect.Field;
+import java.lang.reflect.Type;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * <h3>全局捕获异常处理</h3> <br>
@@ -31,128 +41,103 @@ import java.util.Map;
  */
 public class CrashHandler implements UncaughtExceptionHandler {
 
-    private Context mContext;
-    public static String TAG = "CrashHandler";
-    private String exitInfo = "很抱歉,程序出现异常,即将退出!";
+    public static String TAG = CrashHandler.class.getSimpleName();
 
-    private Thread.UncaughtExceptionHandler mDefaultHandler;// 系统默认的UncaughtException处理类
-    private Map<String, String> info = new HashMap<String, String>();// 用来存储设备信息和异常信息
+    private static final String CACHE_KEY = "cache_report_data";
 
-    private static class HolderClass {
-        private static CrashHandler instance = new CrashHandler();
-    }
+    private static CrashHandler instance;
 
-    /**
-     * 获取CrashHandler实例 ,单例模式
-     */
     public static CrashHandler getInstance() {
-        return HolderClass.instance;
+        if (instance == null) {
+            instance = new CrashHandler();
+        }
+        return instance;
     }
 
-    /**
-     * 初始化
-     *
-     * @param context 上下文
-     */
-    public void init(Context context) {
-        mContext = context;
-        // 获取系统默认的UncaughtException处理器
-        mDefaultHandler = Thread.getDefaultUncaughtExceptionHandler();
-        // 设置该CrashHandler为程序的默认处理器
+    private Thread.UncaughtExceptionHandler defaultCrashHandler;
+
+    private CrashHandler() {
+        defaultCrashHandler = Thread.getDefaultUncaughtExceptionHandler();
+    }
+
+    public void init() {
         Thread.setDefaultUncaughtExceptionHandler(this);
+        uploadCachedLog();
     }
 
-    /**
-     * 当UncaughtException发生时会转入该函数来处理
-     */
     @Override
-    public void uncaughtException(Thread thread, Throwable ex) {
-        try {
-            if (!handleException(ex) && mDefaultHandler != null) {
-                // 如果用户没有处理则让系统默认的异常处理器来处理
-                mDefaultHandler.uncaughtException(thread, ex);
-            } else {
-                SystemClock.sleep(3000);
-                // 退出程序
-                android.os.Process.killProcess(android.os.Process.myPid());
-                System.exit(1);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void uncaughtException(@NonNull Thread t, @NonNull Throwable e) {
+        Map<String, Object> info = new HashMap<>();
+        info.put("thread", t.getName());
+        info.put("time", DateUtil.formatDate("yyyy-MM-dd HH:mm:ss", System.currentTimeMillis()));
+        info.put("app_info", collectAppVersionInfo());
+        info.put("device_info", collectDeviceInfo());
+        info.put("network_info", collectNetworkInfo());
+        info.put("stack_trace", getStackTraceStr(e));
+        submitLog(info)
+        .doOnError(ex -> {
+            saveToLocal(info);
+            defaultCrashHandler.uncaughtException(t, e);
+        })
+        .doOnNext(r -> defaultCrashHandler.uncaughtException(t, e))
+        .subscribe(new RxSubscriber<>());
     }
 
-    /**
-     * 自定义错误处理,收集错误信息 发送错误报告等操作均在此完成.
-     *
-     * @param ex Throwable
-     * @return true:如果处理了该异常信息;false:否则返回
-     */
-    private boolean handleException(Throwable ex) throws Exception {
-        if (ex == null)
-            return false;
-        // 使用Toast来显示异常信息
-        new Thread() {
-            @Override
-            public void run() {
-                Looper.prepare();
-                Toast.makeText(mContext, exitInfo, Toast.LENGTH_LONG).show();
-                Looper.loop();
-            }
-        }.start();
-        // 收集设备参数信息
-        collectDeviceInfo(mContext);
-        // 保存日志文件
-        saveCrashInfoFile(ex);
-
-        return true;
-    }
-
-    /**
-     * 收集设备参数信息
-     *
-     * @param ctx ctx
-     */
-    public void collectDeviceInfo(Context ctx) throws Exception {
+    private Map<String, Object> collectAppVersionInfo() {
+        Map<String, Object> ret = new ArrayMap<>();
         try {
-            PackageManager pm = ctx.getPackageManager();
-            PackageInfo pi = pm.getPackageInfo(ctx.getPackageName(),
-                    PackageManager.GET_ACTIVITIES);
+            PackageManager pm = ContextProvider.get().getPackageManager();
+            PackageInfo pi = pm.getPackageInfo(ContextProvider.get().getPackageName(), PackageManager.GET_ACTIVITIES);
             if (pi != null) {
-                String versionName = pi.versionName + "";
-                String versionCode = pi.versionCode + "";
-                info.put("versionName", versionName);
-                info.put("versionCode", versionCode);
+                ret.put("versionName", pi.versionName);
+                ret.put("versionCode", pi.versionCode);
             }
-        } catch (NameNotFoundException e) {
-            Log.i(TAG, "an error occurred when collect package info", e);
-        }
+        } catch (Exception ignored) {}
+//        ret.put("debug_mode", GlobalConfig.isDebugMode());
+//        ret.put("beta_mode", GlobalConfig.isBetaMode());
+        return ret;
+    }
+
+    private Map<String, Object> collectDeviceInfo() {
+        Map<String, Object> ret = new ArrayMap<>();
+
+        ret.put("MODEL", Build.MODEL);
+        ret.put("ID", Build.ID);
+        ret.put("HOST", Build.HOST);
+        ret.put("OS_VERSION", Build.VERSION.RELEASE + "(API" + Build.VERSION.SDK_INT + ")");
+        ret.put("DISPLAY", Build.DISPLAY);
+        ret.put("PRODUCT", Build.PRODUCT);
+        ret.put("MANUFACTURER", Build.MANUFACTURER);
+        ret.put("CPU_ABI", Build.CPU_ABI);
+        ret.put("SUPPORTED_ABIS", Arrays.asList(Build.SUPPORTED_ABIS));
+        List<String> extraInfo = Arrays.asList("SOC_MODEL", "SOC_MANUFACTURER", "IS_EMULATOR");
         Field[] fields = Build.class.getDeclaredFields();
         for (Field field : fields) {
-            try {
-                field.setAccessible(true);
-                info.put(field.getName(), field.get(null).toString());
-            } catch (Exception e) {
-                Log.e(TAG, "an error occurred when collect crash info", e);
+            if (extraInfo.contains(field.getName())) {
+                try {
+                    field.setAccessible(true);
+                    ret.put(field.getName(), String.valueOf(field.get(null)));
+                } catch (Exception ignored) { }
             }
         }
+        return ret;
     }
 
-    /**
-     * 保存错误信息到文件中
-     *
-     * @param ex Throwable
-     * @throws IOException 输入输出异常
-     */
-    private void saveCrashInfoFile(Throwable ex) throws IOException {
-        StringBuilder sb = new StringBuilder();
-
-        for (Map.Entry<String, String> entry : info.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
-            sb.append(key).append("=").append(value).append("\n");
+    private Map<String, Object> collectNetworkInfo() {
+        Map<String, Object> ret = new ArrayMap<>();
+        ConnectivityManager manager = (ConnectivityManager) ContextProvider.get().getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (manager == null) {
+            return ret;
         }
+        NetworkInfo activeNetworkInfo = manager.getActiveNetworkInfo();
+        ret.put("connected", activeNetworkInfo.isConnected());
+        ret.put("type", activeNetworkInfo.getTypeName());
+        ret.put("detail_state", activeNetworkInfo.getDetailedState().toString());
+        ret.put("extra_info", activeNetworkInfo.getExtraInfo());
+        return ret;
+    }
 
+    private String getStackTraceStr(Throwable ex) {
         Writer writer = new StringWriter();
         PrintWriter printWriter = new PrintWriter(writer);
         ex.printStackTrace(printWriter);
@@ -164,9 +149,35 @@ public class CrashHandler implements UncaughtExceptionHandler {
         printWriter.flush();
         printWriter.close();
         String result = writer.toString();
-        sb.append(result);
-        Log.e(TAG, sb.toString());
-        String destPath = ContextProvider.get().getExternalCacheDir() + File.separator + "error_logs";
-        FileUtil.writeToFile(destPath, sb.toString());
+        Log.e(TAG, result);
+        return result;
+    }
+
+    private void saveToLocal(Map<String, Object> info) {
+        JSONObject reportData = GsonParser.toJSONObject(info);
+        if (reportData != null) {
+            AppPreferences.getDefault().put(CACHE_KEY, reportData.toString());
+        }
+    }
+
+    private void uploadCachedLog() {
+        String cacheStr = AppPreferences.getDefault().getString(CACHE_KEY, "");
+        Log.d(TAG, "cached log: " + cacheStr);
+        if (TextUtils.isEmpty(cacheStr)) {
+            return;
+        }
+        Type convertType = new TypeToken<Map<String, Object>>() {}.getType();
+        Map<String, Object> reportData = GsonParser.getParser().fromJson(cacheStr, convertType);
+        if (reportData == null) {
+            return;
+        }
+        AppPreferences.getDefault().remove(CACHE_KEY);
+        submitLog(reportData).subscribe(new RxSubscriber<>());
+    }
+
+    private Observable<Object> submitLog(Map<String, Object> info) {
+        return RetrofitManager.getCommonApi().jsonPost("", info)
+        .subscribeOn(Schedulers.io())
+        .map(resp -> resp);
     }
 }
