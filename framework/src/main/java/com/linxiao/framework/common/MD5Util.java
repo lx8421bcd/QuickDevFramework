98eb2b10d29b4865328f66d40c1e523a642fa1a4
lx8421bcd <lx8421bcd@github.com>
2022-12-07 14:11:17 +0800

    refactor: add 16bit md5 string support

--- framework/src/main/java/com/linxiao/framework/common/MD5Util.java
+++ framework/src/main/java/com/linxiao/framework/common/MD5Util.java
@@ -1,55 +1,54 @@
 package com.linxiao.framework.common;
 
-import android.util.Log;
-
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Locale;
 
 /**
+ * MD5 string generate tool
  *
- * Created by linxiao on 2016-07-28.
+ * @author lx8421bcd
+ * @since 2016-07-28
  */
 public class MD5Util {
 
-    public static final int MD5_UPPER_CASE = 0;
-    public static final int MD5_LOWER_CASE = 1;
+    public enum CASE {
+        LOWER, UPPER
+    }
 
     private MD5Util() {}
 
-    /**
-     * MD5加密
-     */
-    public static String getMD5Str(String str, int caseCode) {
-        MessageDigest messageDigest = null;
-
+    public static String getMD5String32(String str, CASE caseType) {
         try {
-            messageDigest = MessageDigest.getInstance("MD5");
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
             messageDigest.reset();
-            messageDigest.update(str.getBytes("UTF-8"));
+            messageDigest.update(str.getBytes(StandardCharsets.UTF_8));
+
+            byte[] byteArray = messageDigest.digest();
+            StringBuilder md5StrBuff = new StringBuilder();
+
+            for (byte aByteArray : byteArray) {
+                if (Integer.toHexString(0xFF & aByteArray).length() == 1)
+                    md5StrBuff.append("0").append(Integer.toHexString(0xFF & aByteArray));
+                else
+                    md5StrBuff.append(Integer.toHexString(0xFF & aByteArray));
+            }
+            switch (caseType) {
+                case LOWER:
+                    return md5StrBuff.toString().toLowerCase(Locale.getDefault());
+                case UPPER:
+                    return md5StrBuff.toString().toUpperCase(Locale.getDefault());
+            }
+            return md5StrBuff.toString().toUpperCase(Locale.getDefault());
         } catch (NoSuchAlgorithmException e) {
-            Log.e("MD5Util", "NoSuchAlgorithmException caught!");
-            System.exit(-1);
-        } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
+        return "";
+    }
 
-        byte[] byteArray = messageDigest.digest();
-        StringBuilder md5StrBuff = new StringBuilder();
-
-        for (byte aByteArray : byteArray) {
-            if (Integer.toHexString(0xFF & aByteArray).length() == 1)
-                md5StrBuff.append("0").append(Integer.toHexString(0xFF & aByteArray));
-            else
-                md5StrBuff.append(Integer.toHexString(0xFF & aByteArray));
-        }
-        switch (caseCode) {
-            case MD5_LOWER_CASE :
-                return md5StrBuff.toString().toLowerCase(Locale.getDefault());
-            case MD5_UPPER_CASE :
-                return md5StrBuff.toString().toUpperCase(Locale.getDefault());
-        }
-        return md5StrBuff.toString().toUpperCase(Locale.getDefault());
+    public static String getMD5String16(String str, CASE caseType) {
+        String md5Str = getMD5String32(str, caseType);
+        return md5Str.isEmpty() ? md5Str : md5Str.substring(8, 24);
     }
 }
