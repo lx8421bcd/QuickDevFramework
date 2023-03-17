71501adff973f95890e0d2e8a2c1185588da9d3a
lx8421bcd <lx8421bcd@github.com>
2023-03-17 18:30:35 +0800

    splashscreen sdk接入

--- app/src/main/java/com/linxiao/quickdevframework/main/SplashActivity.java
+++ app/src/main/java/com/linxiao/quickdevframework/main/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.linxiao.quickdevframework.main;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -7,22 +8,30 @@ import android.os.Handler;
 import com.linxiao.framework.architecture.BaseSplashActivity;
 import com.linxiao.quickdevframework.R;
 
+@SuppressLint("CustomSplashScreen")
 public class SplashActivity extends BaseSplashActivity {
+
+    private boolean dataReady = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        keepSplashScreenUntilInitFinished();
+        execInit();
+    }
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
+    private void execInit() {
+        new Handler().postDelayed(() -> {
+            dataReady = true;
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
         }, 1000);
     }
 
-
+    @Override
+    protected boolean isInitFinished() {
+        return dataReady;
+    }
 }
--- framework/src/main/java/com/linxiao/framework/architecture/BaseSplashActivity.java
+++ framework/src/main/java/com/linxiao/framework/architecture/BaseSplashActivity.java
@@ -1,12 +1,18 @@
 package com.linxiao.framework.architecture;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
+import androidx.core.splashscreen.SplashScreen;
+
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewTreeObserver;
 
 import com.linxiao.framework.notification.NotificationManager;
+
 
 /**
  * 启动Activity基类
@@ -14,13 +20,16 @@ import com.linxiao.framework.notification.NotificationManager;
  * </p>
  * Created by linxiao on 2016/12/5.
  */
+@SuppressLint("CustomSplashScreen")
 public abstract class BaseSplashActivity extends BaseActivity {
 
     protected boolean isHandleNotification;
     private Bundle notificationExtra;
+    protected SplashScreen splashScreen;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        splashScreen = SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
         notificationExtra = intent.getBundleExtra(NotificationManager.KEY_NOTIFICATION_EXTRA);
@@ -52,6 +61,31 @@ public abstract class BaseSplashActivity extends BaseActivity {
             e.printStackTrace();
             Log.e(TAG, "handleNotification: reflect to get activity class failed !");
         }
+    }
+
+    protected boolean isInitFinished() {
+        return true;
+    }
+
+    protected void keepSplashScreenUntilInitFinished() {
+        // Set up an OnPreDrawListener to the root view.
+        final View content = findViewById(android.R.id.content);
+        content.getViewTreeObserver().addOnPreDrawListener(
+                new ViewTreeObserver.OnPreDrawListener() {
+                    @Override
+                    public boolean onPreDraw() {
+                        // Check if the initial data is ready.
+                        if (isInitFinished()) {
+                            // The content is ready; start drawing.
+                            content.getViewTreeObserver().removeOnPreDrawListener(this);
+                            return true;
+                        } else {
+                            // The content is not ready; suspend.
+                            return false;
+                        }
+                    }
+                }
+        );
     }
 
     @Override
