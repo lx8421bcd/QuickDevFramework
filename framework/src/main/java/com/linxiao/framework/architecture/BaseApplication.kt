332082725ccdf55df8dee7b7dca32bfcb5003e27
lx8421bcd <lx8421bcd@github.com>
2023-10-13 11:52:09 +0800

    feature: add method for check isAppForeground with Android Lifecycle

--- framework/src/main/java/com/linxiao/framework/architecture/BaseApplication.kt
+++ framework/src/main/java/com/linxiao/framework/architecture/BaseApplication.kt
@@ -1,29 +1,54 @@
-package com.linxiao.framework.architecture;
+package com.linxiao.framework.architecture
 
-import android.content.res.Resources;
-
-import androidx.multidex.MultiDexApplication;
-
-import com.linxiao.framework.common.DensityHelper;
-import com.linxiao.framework.language.AppLanguageHelper;
+import android.content.res.Resources
+import android.util.Log
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ProcessLifecycleOwner
+import androidx.multidex.MultiDexApplication
+import com.linxiao.framework.common.DensityHelper.onActivityGetResources
+import com.linxiao.framework.language.AppLanguageHelper
 
 /**
  * Base Application class in framework
  *
+ * @author lx8421bcd
+ * @since 2018-06-05
  */
-public abstract class BaseApplication extends MultiDexApplication {
+abstract class BaseApplication : MultiDexApplication(), LifecycleEventObserver {
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-
+    companion object {
+        /**
+         * 检查应用是否处于前台
+         * <p>
+         * 使用LifeCycleEventObserver监测实现，更为安全和精确
+         * </p>
+         *
+         * @return is app foreground
+         */
+        @JvmStatic
+        var isAppForeground: Boolean = false
+            private set
     }
 
-    @Override
-    public Resources getResources() {
-        Resources res = super.getResources();
-        DensityHelper.onActivityGetResources(res);
-        AppLanguageHelper.doOnContextGetResources(res);
-        return res;
+    val TAG = this.javaClass.simpleName
+
+    override fun onCreate() {
+        super.onCreate()
+        ProcessLifecycleOwner.get().lifecycle.addObserver(this)
     }
+
+    override fun getResources(): Resources {
+        val res = super.getResources()
+        onActivityGetResources(res)
+        AppLanguageHelper.doOnContextGetResources(res)
+        return res
+    }
+
+    override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+        Log.d(TAG, "Application Lifecycle onStateChanged: ${event.name}")
+        isAppForeground = source.lifecycle.currentState.isAtLeast(Lifecycle.State.STARTED)
+    }
+
 }
