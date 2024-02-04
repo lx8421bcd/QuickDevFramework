8ea96487699df03a32387f1dd617f41b3a2be06f
lx8421bcd <lx8421bcd@github.com>
2024-02-04 16:28:56 +0800

    chore: change DateUtil to kotlin implementation

--- framework/src/main/java/com/linxiao/framework/common/DateUtil.kt
+++ framework/src/main/java/com/linxiao/framework/common/DateUtil.kt
@@ -1,13 +1,21 @@
-package com.linxiao.framework.common;
+package com.linxiao.framework.common
 
-import android.text.TextUtils;
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
-import java.util.TimeZone;
+const val YEAR = "yyyy"
+const val MONTH = "MM"
+const val DAY = "dd"
+const val WEEK = "EEE"
+const val HOUR_FORMAT_24 = "HH"
+const val HOUR_FORMAT_12 = "hh"
+const val MINUTE = "mm"
+const val SECOND = "ss"
+const val MILLISECOND = "SSS"
 
 /**
  * date tools
@@ -15,178 +23,164 @@ import java.util.TimeZone;
  * @author linxiao
  * @since 2014/8/21
  */
-public class DateUtil {
-	private static final String TAG = DateUtil.class.getSimpleName();
+object DateUtil {
+    private val TAG = DateUtil::class.java.simpleName
+    /*常用时间单位毫秒数，到天，再往上没有意义了，请通过日历获取*/
+    /**一秒的毫秒数 */
+    const val MS_ONE_SECOND: Long = 1000
 
-	public static final String YEAR = "yyyy";
-	public static final String MONTH = "MM";
-	public static final String DAY = "dd";
-	public static final String WEEK = "EEE";
-	public static final String HOUR_FORMAT_24 = "HH";
-	public static final String HOUR_FORMAT_12 = "hh";
-	public static final String MINUTE = "mm";
-	public static final String SECOND = "ss";
-	public static final String MILLISECOND = "SSS";
+    /**一分钟的毫秒数 */
+    const val MS_ONE_MINUTE = MS_ONE_SECOND * 60
 
-	/*常用时间单位毫秒数，到天，再往上没有意义了，请通过日历获取*/
-	/**一秒的毫秒数*/
-	public static final long MS_ONE_SECOND = 1000;
-	/**一分钟的毫秒数*/
-	public static final long MS_ONE_MINUTE = MS_ONE_SECOND * 60;
-	/**一小时的毫秒数*/
-	public static final long MS_ONE_HOUR = MS_ONE_MINUTE * 60;
-	/**一天的毫秒数*/
-	public static final long MS_ONE_DAY = MS_ONE_HOUR * 24;
-	/**一分钟的秒数*/
-	public static final long SEC_ONE_MINUTE = MS_ONE_MINUTE / MS_ONE_SECOND;
-	/**一小时的秒数*/
-	public static final long SEC_ONE_HOUR = MS_ONE_HOUR / MS_ONE_SECOND;
-	/**一天的秒数*/
-	public static final long SEC_ONE_DAY = MS_ONE_DAY / MS_ONE_SECOND;
+    /**一小时的毫秒数 */
+    const val MS_ONE_HOUR = MS_ONE_MINUTE * 60
 
-	/**
-	 * 获取今日0点时间戳，单位毫秒
-	 * @return timestamp in milliseconds
-	 */
-	public static long getZeroTimeMillsOfToday() {
-		Calendar calendar = Calendar.getInstance();
-		calendar.set(Calendar.HOUR_OF_DAY, 0);
-		calendar.set(Calendar.MINUTE, 0);
-		calendar.set(Calendar.SECOND, 0);
-		calendar.set(Calendar.MILLISECOND, 0);
-		return calendar.getTimeInMillis();
-	}
+    /**一天的毫秒数 */
+    const val MS_ONE_DAY = MS_ONE_HOUR * 24
 
-	/**
-	 * 获取今日0点时间戳，单位秒
-	 * @return timestamp in milliseconds
-	 */
-	public static long getZeroTimeSecOfToday() {
-		return getZeroTimeMillsOfToday() / 1000;
-	}
+    /**一分钟的秒数 */
+    const val SEC_ONE_MINUTE = MS_ONE_MINUTE / MS_ONE_SECOND
 
-	public static long currentTimeMillis() {
-		return System.currentTimeMillis();
-	}
+    /**一小时的秒数 */
+    const val SEC_ONE_HOUR = MS_ONE_HOUR / MS_ONE_SECOND
 
-	public static long currentTimeSec() {
-		return System.currentTimeMillis() / 1000;
-	}
+    /**一天的秒数 */
+    const val SEC_ONE_DAY = MS_ONE_DAY / MS_ONE_SECOND
 
-	public static String currentTimeString(String format) {
-		return timeMillsToDateString(format, currentTimeMillis());
-	}
+    /**
+     * 获取今日0点时间戳，单位毫秒
+     * @return timestamp in milliseconds
+     */
+    val zeroTimeMillisOfToday: Long
+        get() {
+            val calendar = Calendar.getInstance()
+            calendar[Calendar.HOUR_OF_DAY] = 0
+            calendar[Calendar.MINUTE] = 0
+            calendar[Calendar.SECOND] = 0
+            calendar[Calendar.MILLISECOND] = 0
+            return calendar.timeInMillis
+        }
 
-	public static Date currentDate() {
-		Calendar calendar = Calendar.getInstance();
-		calendar.setTimeZone(TimeZone.getDefault());
-		return calendar.getTime();
-	}
+    /**
+     * 获取今日0点时间戳，单位秒
+     * @return timestamp in milliseconds
+     */
+    val zeroTimeSecOfToday: Long
+        get() = zeroTimeMillisOfToday / 1000
 
-	public static long getPastTimeMillsToday() {
-		return currentTimeMillis() - getZeroTimeMillsOfToday();
-	}
+    fun currentTimeMillis(): Long {
+        return System.currentTimeMillis()
+    }
 
-	public static long getPastTimeSecToday() {
-		return getPastTimeMillsToday() / 1000;
-	}
+    fun currentTimeSec(): Long {
+        return System.currentTimeMillis() / 1000
+    }
 
-	/**
-	 * convert date string to timestamp
-	 *
-	 * @param timeZone timezone, default using current timezone
-	 * @param format date string format
-	 * @param dateString date string
-	 * @return timestamp in milliseconds
-	 */
-	public static long dateStringToTimeMills(String timeZone, String format, String dateString) {
-		SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.getDefault());
-		if (!TextUtils.isEmpty(timeZone)) {
-			sdf.setTimeZone(TimeZone.getTimeZone(timeZone));
-		}
-		try {
-			Date date = sdf.parse(dateString);
-			return date.getTime();
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		return 0;
-	}
+    fun currentTimeString(format: String?): String? {
+        return timeMillsToDateString(format, currentTimeMillis())
+    }
 
-	/**
-	 * convert date string to timestamp
-	 *
-	 * @param format date string format
-	 * @param dateString date string
-	 * @return date string: timestamp in milliseconds; time string: past milliseconds from today
-	 */
-	public static long dateStringToTimeMills(String format, String dateString) {
-		// 日期格式的时间默认使用当前时区返回时间戳
-		if (format.contains(YEAR) || format.contains(MONTH) || format.contains(DAY) || format.contains(WEEK)) {
-			return dateStringToTimeMills(null, format, dateString);
-		}
-		return dateStringToTimeMills("GMT", format, dateString);
-	}
+    fun currentDate(): Date {
+        val calendar = Calendar.getInstance()
+        calendar.timeZone = TimeZone.getDefault()
+        return calendar.time
+    }
 
-	/**
-	 * convert date string to timestamp
-	 *
-	 * @param format date string format
-	 * @param dateString date string
-	 * @return date string: timestamp in seconds; time string: past seconds from today
-	 */
-	public static long dateStringToTimeSec(String format, String dateString) {
-		return dateStringToTimeMills(format, dateString) / 1000;
-	}
+    val pastTimeMillisToday: Long
+        get() = currentTimeMillis() - zeroTimeMillisOfToday
 
-	/**
-	 * convert timestamp in milliseconds to string
-	 *
-	 * @param format date string format
-	 * @param timeMills date object
-	 * @return date string
-	 */
-	public static String timeMillsToDateString(String format, long timeMills) {
-		SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.getDefault());
-		try {
-			return sdf.format(new Date(timeMills));
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
+    val pastTimeSecToday: Long
+        get() = pastTimeMillisToday / 1000
 
-	/**
-	 * check if the current time is within the defined time period
-	 * 当前是否在指定时间段内
-	 *
-	 * @param start	开始时间戳
-	 * @param end	结束时间戳
-	 * @return true 在时间段内， false 不在时间段内
-	 */
-	public static boolean isInThePeriod(long start, long end) {
-		long cur = currentTimeMillis();
-		return start <= cur && cur <= end;
-	}
+    /**
+     * 检查格式化字符串是否包含日期
+     */
+    fun isFormatContainDate(format: String): Boolean {
+        val pattern = SimpleDateFormat(format, Locale.getDefault()).toPattern().lowercase()
+        return pattern.contains("y") || pattern.contains("m") || pattern.contains("d")
+    }
 
-	public static boolean isInThePeriod(String format, long start, String end) {
-		return isInThePeriod(
-				start,
-				dateStringToTimeMills(format, end)
-		);
-	}
+    /**
+     * convert date string to timestamp
+     *
+     * @param timeZone timezone, default using current timezone
+     * @param format date string format
+     * @param dateString date string
+     * @return timestamp in milliseconds
+     */
+    fun dateStringToTimeMillis(
+        dateString: String,
+        format: String = "yyyy-MM-dd HH:mm:ss",
+        timeZone: String = "",
+    ): Long {
+        val sdf = SimpleDateFormat(format, Locale.getDefault())
+        var setTimeZone = timeZone
+        if (setTimeZone.isEmpty() && !isFormatContainDate(format)) {
+            setTimeZone = "GMT"
+        }
+        // 不包含日期的时间默认使用当前时区返回时间戳
+        if (setTimeZone.isNotEmpty()) {
+            sdf.timeZone = TimeZone.getTimeZone(setTimeZone)
+        }
+        try {
+            val date = sdf.parse(dateString)
+            return date?.time ?: 0
+        } catch (e: ParseException) {
+            e.printStackTrace()
+        }
+        return 0
+    }
 
-	public static boolean isInThePeriod(String format, String start, long end) {
-		return isInThePeriod(
-				dateStringToTimeMills(format, start),
-				end
-		);
-	}
-	public static boolean isInThePeriod(String format, String start, String end) {
-		return isInThePeriod(
-				dateStringToTimeMills(format, start),
-				dateStringToTimeMills(format, end)
-		);
-	}
+    /**
+     * convert date string to timestamp
+     *
+     * @param format date string format
+     * @param dateString date string
+     * @return date string: timestamp in seconds; time string: past seconds from today
+     */
+    fun dateStringToTimeSec(
+        dateString: String,
+        format: String = "yyyy-MM-dd HH:mm:ss",
+        timeZone: String = "",
+    ): Long {
+        return dateStringToTimeMillis(dateString, format, timeZone) / 1000
+    }
 
+    /**
+     * convert timestamp in milliseconds to string
+     *
+     * @param format date string format
+     * @param timeMills date object
+     * @return date string
+     */
+    @JvmStatic
+    fun timeMillsToDateString(format: String?, timeMills: Long): String? {
+        val sdf = SimpleDateFormat(format, Locale.getDefault())
+        return try {
+            sdf.format(Date(timeMills))
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+
+    /**
+     * check if the current time is within the defined time period
+     * 当前是否在指定时间段内
+     *
+     * @param start    开始时间戳
+     * @param end    结束时间戳
+     * @return true 在时间段内， false 不在时间段内
+     */
+    fun isInPeriod(start: Long, end: Long): Boolean {
+        val cur = currentTimeMillis()
+        return cur in start..end
+    }
+
+    fun isInPeriod(format: String, start: String, end: String): Boolean {
+        return isInPeriod(
+            dateStringToTimeMillis(start, format),
+            dateStringToTimeMillis(end, format)
+        )
+    }
 }
