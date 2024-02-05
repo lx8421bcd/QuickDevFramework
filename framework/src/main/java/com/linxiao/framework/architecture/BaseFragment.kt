7fd70fd247e232b62ad8e7caa5a29cec7bb428a5
lx8421bcd <lx8421bcd@github.com>
2024-02-05 16:05:38 +0800

    feature: turned BaseFragment to kotlin impl

--- framework/src/main/java/com/linxiao/framework/architecture/BaseFragment.kt
+++ framework/src/main/java/com/linxiao/framework/architecture/BaseFragment.kt
@@ -1,129 +1,111 @@
-package com.linxiao.framework.architecture;
+package com.linxiao.framework.architecture
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.annotation.CheckResult;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
-import com.trello.rxlifecycle2.LifecycleProvider;
-import com.trello.rxlifecycle2.LifecycleTransformer;
-import com.trello.rxlifecycle2.RxLifecycle;
-import com.trello.rxlifecycle2.android.FragmentEvent;
-import com.trello.rxlifecycle2.android.RxLifecycleAndroid;
-
-import io.reactivex.Observable;
-import io.reactivex.subjects.BehaviorSubject;
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.annotation.CheckResult
+import androidx.fragment.app.Fragment
+import com.linxiao.framework.architecture.ActivityResultHolderFragment.Companion.startActivityForCallback
+import com.trello.rxlifecycle2.LifecycleProvider
+import com.trello.rxlifecycle2.LifecycleTransformer
+import com.trello.rxlifecycle2.RxLifecycle
+import com.trello.rxlifecycle2.android.FragmentEvent
+import com.trello.rxlifecycle2.android.RxLifecycleAndroid
+import io.reactivex.Observable
+import io.reactivex.subjects.BehaviorSubject
 
 /**
  * base Fragment class of entire project
- * <p>
+ *
+ *
  * template for Fragments in the project, contains common methods.
  * extends from Android base appcompat component class, manually implemented the implementation
  * of RxLifeCycle, if you have to extends framework base class from some third sdk, just
  * change parent class is ok.
- * </p>
+ *
  *
  * @author linxiao
  * @since 2016-12-05
  */
-public abstract class BaseFragment extends Fragment implements LifecycleProvider<FragmentEvent> {
-    protected String TAG;
+abstract class BaseFragment : Fragment(), LifecycleProvider<FragmentEvent> {
 
-    private final BehaviorSubject<FragmentEvent> lifecycleSubject = BehaviorSubject.create();
+    @JvmField
+    protected var TAG: String = this.javaClass.simpleName
 
-    public BaseFragment() {
-        TAG = this.getClass().getSimpleName();
+    private val lifecycleSubject = BehaviorSubject.create<FragmentEvent>()
+
+    fun startActivityForCallback(
+        intent: Intent?,
+        callback: ActivityResultCallback<ActivityResult>?
+    ) {
+        this.startActivityForCallback(intent, callback, null)
     }
 
-    public void startActivityForCallback(Intent intent, ActivityResultCallback<ActivityResult> callback) {
-        ActivityResultHolderFragment.startActivityForCallback(this, intent, callback, null);
-    }
-
-    @Override
-    @NonNull
     @CheckResult
-    public final Observable<FragmentEvent> lifecycle() {
-        return lifecycleSubject.hide();
+    override fun lifecycle(): Observable<FragmentEvent> {
+        return lifecycleSubject.hide()
     }
 
-    @Override
-    @NonNull
     @CheckResult
-    public final <T> LifecycleTransformer<T> bindUntilEvent(@NonNull FragmentEvent event) {
-        return RxLifecycle.bindUntilEvent(lifecycleSubject, event);
+    override fun <T> bindUntilEvent(event: FragmentEvent): LifecycleTransformer<T> {
+        return RxLifecycle.bindUntilEvent(lifecycleSubject, event)
     }
 
-    @Override
-    @NonNull
     @CheckResult
-    public final <T> LifecycleTransformer<T> bindToLifecycle() {
-        return RxLifecycleAndroid.bindFragment(lifecycleSubject);
+    override fun <T> bindToLifecycle(): LifecycleTransformer<T> {
+        return RxLifecycleAndroid.bindFragment(lifecycleSubject)
     }
 
-    @Override
-    public void onAttach(android.app.Activity activity) {
-        super.onAttach(activity);
-        lifecycleSubject.onNext(FragmentEvent.ATTACH);
+    override fun onAttach(activity: Activity) {
+        super.onAttach(activity)
+        lifecycleSubject.onNext(FragmentEvent.ATTACH)
     }
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        lifecycleSubject.onNext(FragmentEvent.CREATE);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        lifecycleSubject.onNext(FragmentEvent.CREATE)
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        lifecycleSubject.onNext(FragmentEvent.CREATE_VIEW);
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        lifecycleSubject.onNext(FragmentEvent.CREATE_VIEW)
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        lifecycleSubject.onNext(FragmentEvent.START);
+    override fun onStart() {
+        super.onStart()
+        lifecycleSubject.onNext(FragmentEvent.START)
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        lifecycleSubject.onNext(FragmentEvent.RESUME);
+    override fun onResume() {
+        super.onResume()
+        lifecycleSubject.onNext(FragmentEvent.RESUME)
     }
 
-    @Override
-    public void onPause() {
-        lifecycleSubject.onNext(FragmentEvent.PAUSE);
-        super.onPause();
+    override fun onPause() {
+        lifecycleSubject.onNext(FragmentEvent.PAUSE)
+        super.onPause()
     }
 
-    @Override
-    public void onStop() {
-        lifecycleSubject.onNext(FragmentEvent.STOP);
-        super.onStop();
+    override fun onStop() {
+        lifecycleSubject.onNext(FragmentEvent.STOP)
+        super.onStop()
     }
 
-    @Override
-    public void onDestroyView() {
-        lifecycleSubject.onNext(FragmentEvent.DESTROY_VIEW);
-        super.onDestroyView();
+    override fun onDestroyView() {
+        lifecycleSubject.onNext(FragmentEvent.DESTROY_VIEW)
+        super.onDestroyView()
     }
 
-    @Override
-    public void onDestroy() {
-        lifecycleSubject.onNext(FragmentEvent.DESTROY);
-        super.onDestroy();
+    override fun onDestroy() {
+        lifecycleSubject.onNext(FragmentEvent.DESTROY)
+        super.onDestroy()
     }
 
-    @Override
-    public void onDetach() {
-        lifecycleSubject.onNext(FragmentEvent.DETACH);
-        super.onDetach();
+    override fun onDetach() {
+        lifecycleSubject.onNext(FragmentEvent.DETACH)
+        super.onDetach()
     }
-
 }
