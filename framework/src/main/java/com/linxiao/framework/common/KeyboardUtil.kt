2b55aec56d9b01f89a0febddece5596577e80faa
lx8421bcd <lx8421bcd@github.com>
2024-02-18 16:48:40 +0800

    feature: convert KeyboardUtil methods to kotlin extension method impl

--- framework/src/main/java/com/linxiao/framework/common/KeyboardUtil.kt
+++ framework/src/main/java/com/linxiao/framework/common/KeyboardUtil.kt
@@ -10,28 +10,20 @@ import android.view.inputmethod.InputMethodManager
  * @author 0x8421bcd
  * @since 2022-08-31
  */
-object KeyboardUtil {
 
-    @JvmStatic
-    fun hideKeyboard(targetView: View?) {
-        targetView ?: return
-        targetView.clearFocus()
-        val imm = targetView.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(targetView.windowToken, 0)
-    }
+fun View.hideKeyboard() {
+    this.clearFocus()
+    val imm = this.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(this.windowToken, 0)
+}
 
-    @JvmStatic
-    fun showKeyboard(targetView: View?) {
-        targetView ?: return
-        val imm = targetView.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        targetView.requestFocus()
-        imm.showSoftInput(targetView, InputMethodManager.SHOW_IMPLICIT)
-    }
+fun View.showKeyboard() {
+    val imm = this.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    this.requestFocus()
+    imm.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+}
 
-    @JvmStatic
-    fun isSoftKeyBoardActiveFor(targetView: View?): Boolean {
-        targetView ?: return false
-        val imm = targetView.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        return imm.isActive(targetView)
-    }
+fun View.isKeyboardActiveFor(): Boolean {
+    val imm = this.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    return imm.isActive(this)
 }
