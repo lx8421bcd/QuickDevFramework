728718d79d8fc7a72e27255629295eb0bf880293
lx8421bcd <lx8421bcd@github.com>
2024-03-14 17:52:26 +0800

    refactor: Preferences convert to kotlin impl

--- framework/src/main/java/com/linxiao/framework/preferences/AppPreferences.kt
+++ framework/src/main/java/com/linxiao/framework/preferences/AppPreferences.kt
@@ -1,71 +1,61 @@
-package com.linxiao.framework.preferences;
+package com.linxiao.framework.preferences
 
-import android.content.Context;
-import android.content.SharedPreferences;
-
-import androidx.annotation.Nullable;
-import androidx.preference.PreferenceManager;
-
-import com.linxiao.framework.common.ContextProviderKt;
-
+import android.content.Context
+import androidx.preference.PreferenceManager
+import com.linxiao.framework.common.globalContext
 
 /**
  * SharedPreferences封装
- * <p>与PreferencesOperateObject配合封装SharedPreferences, 简化SharedPreferences存取操作</p>
  *
- * Created by linxiao on 2014/8/21.
- * */
-public class AppPreferences {
-
-    private AppPreferences() {}
+ * 与PreferencesOperateObject配合封装SharedPreferences, 简化SharedPreferences存取操作
+ *
+ * @author lx8421bcd
+ * @since 2014-8-21.
+ */
+object AppPreferences {
 
     /**
      * 获取默认的SharedPreferences
-     * <p>默认操作模式,代表该文件是私有数据,只能被应用本身访问,在该模式下,写入的内容会覆盖原文件的内容</p>
-     * */
-    public static PreferenceOperator getDefault(Context context) {
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-        return new PreferenceOperator(sharedPreferences);
-    }
-
-    public static PreferenceOperator getDefault() {
-        return getDefault(ContextProviderKt.getGlobalContext());
+     *
+     * 默认操作模式,代表该文件是私有数据,只能被应用本身访问,在该模式下,写入的内容会覆盖原文件的内容
+     */
+    fun getDefault(): PreferenceOperator {
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(globalContext)
+        return PreferenceOperator(sharedPreferences)
     }
 
     /**
      * 获取私有的SharedPreferences
-     * <p>Private模式代表该文件是私有数据,只能被应用本身访问,在该模式下,写入的内容会覆盖原文件的内容</p>
-     * */
-    @Nullable
-    public static PreferenceOperator getPrivate(Context context, String name) {
-        return getPreferencesByMode(context, name, Context.MODE_PRIVATE);
+     *
+     * Private模式代表该文件是私有数据,只能被应用本身访问,在该模式下,写入的内容会覆盖原文件的内容
+     */
+    fun getPrivate(context: Context, name: String): PreferenceOperator? {
+        return getPreferencesByMode(context, name, Context.MODE_PRIVATE)
     }
 
-    public static PreferenceOperator getPrivate(String name) {
-        return getPreferencesByMode(ContextProviderKt.getGlobalContext(), name, Context.MODE_PRIVATE);
+    fun getPrivate(name: String): PreferenceOperator? {
+        return getPreferencesByMode(globalContext, name, Context.MODE_PRIVATE)
     }
 
     /**
      * 获取Append模式的SharedPreferences
-     * <p>Append模式会检查文件是否存在,存在就往文件追加内容,否则就创建新文件.</p>
-     * */
-    @Nullable
-    public static PreferenceOperator getAppend(Context context, String name) {
-        return getPreferencesByMode(context, name, Context.MODE_APPEND);
+     *
+     * Append模式会检查文件是否存在,存在就往文件追加内容,否则就创建新文件.
+     */
+    fun getAppend(context: Context, name: String): PreferenceOperator? {
+        return getPreferencesByMode(context, name, Context.MODE_APPEND)
     }
 
-    public static PreferenceOperator getAppend(String name) {
-        return getPreferencesByMode(ContextProviderKt.getGlobalContext(), name, Context.MODE_APPEND);
+    fun getAppend(name: String): PreferenceOperator? {
+        return getPreferencesByMode(globalContext, name, Context.MODE_APPEND)
     }
 
-
-    @Nullable
-    private static PreferenceOperator getPreferencesByMode(Context context, String name, int mode) {
-        SharedPreferences sharedPreferences = context.getSharedPreferences(name, mode);
-        if (sharedPreferences != null) {
-            return new PreferenceOperator(sharedPreferences);
-        }
-        return null;
+    private fun getPreferencesByMode(
+        context: Context,
+        name: String,
+        mode: Int
+    ): PreferenceOperator? {
+        val sharedPreferences = context.getSharedPreferences(name, mode)
+        return sharedPreferences?.let { PreferenceOperator(it) }
     }
-
 }
--- framework/src/main/java/com/linxiao/framework/preferences/PreferenceOperator.kt
+++ framework/src/main/java/com/linxiao/framework/preferences/PreferenceOperator.kt
@@ -1,147 +1,140 @@
-package com.linxiao.framework.preferences;
+package com.linxiao.framework.preferences
 
-import android.content.SharedPreferences;
-import android.util.Base64;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import android.content.SharedPreferences
+import android.util.Base64
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
+import java.io.Serializable
 
 /**
  * SharedPreferences操作对象
- * <p>用于对SharedPreferences的常见操作进行封装，提供扩展功能</p>
- * Created by linxiao on 2014/08/21.
+ *
+ * 用于对SharedPreferences的常见操作进行封装，提供扩展功能
+ *
+ * @author lx8421bcd
+ * @since 2014-08-21.
  */
-public class PreferenceOperator {
-    private SharedPreferences mPreferences;
-
-    public PreferenceOperator(SharedPreferences preferences) {
-        this.mPreferences = preferences;
+class PreferenceOperator(
+    private val preferences: SharedPreferences
+) {
+    fun clear() {
+        val edit = preferences.edit()
+        edit.clear()
+        edit.apply()
     }
 
-    public void clear() {
-        SharedPreferences.Editor edit = mPreferences.edit();
-        edit.clear();
-        edit.apply();
-    }
-
-    public void remove(String... keys) {
-        if (keys == null) {
-            return;
+    fun remove(vararg keys: String?) {
+        val editor = preferences.edit()
+        for (key in keys) {
+            editor.remove(key)
         }
-        SharedPreferences.Editor editor = mPreferences.edit();
-        for (String key : keys) {
-            editor.remove(key);
-        }
-        editor.apply();
+        editor.apply()
     }
 
-    public boolean checkExist(String key) {
-        return mPreferences.contains(key);
+    fun checkExist(key: String?): Boolean {
+        return preferences.contains(key)
     }
 
-    public void put(String key, boolean value) {
-        SharedPreferences.Editor editor = mPreferences.edit();
-        editor.putBoolean(key, value);
-        editor.apply();
+    fun put(key: String?, value: Boolean) {
+        val editor = preferences.edit()
+        editor.putBoolean(key, value)
+        editor.apply()
     }
 
-    public void put(String key, int value) {
-        SharedPreferences.Editor editor = mPreferences.edit();
-        editor.putInt(key, value);
-        editor.apply();
+    fun put(key: String?, value: Int) {
+        val editor = preferences.edit()
+        editor.putInt(key, value)
+        editor.apply()
     }
 
-    public void put(String key, long value) {
-        SharedPreferences.Editor editor = mPreferences.edit();
-        editor.putLong(key, value);
-        editor.apply();
+    fun put(key: String?, value: Long) {
+        val editor = preferences.edit()
+        editor.putLong(key, value)
+        editor.apply()
     }
 
-    public void put(String key, float value) {
-        SharedPreferences.Editor editor = mPreferences.edit();
-        editor.putFloat(key, value);
-        editor.apply();
+    fun put(key: String?, value: Float) {
+        val editor = preferences.edit()
+        editor.putFloat(key, value)
+        editor.apply()
     }
 
-    public void put(String key, double value) {
-        String valueString = String.valueOf(value);
-        SharedPreferences.Editor editor = mPreferences.edit();
-        editor.putString(key, valueString);
-        editor.apply();
+    fun put(key: String?, value: Double) {
+        val valueString = value.toString()
+        val editor = preferences.edit()
+        editor.putString(key, valueString)
+        editor.apply()
     }
 
-    public void put(String key, String value) {
-        SharedPreferences.Editor editor = mPreferences.edit();
-        editor.putString(key, value);
-        editor.apply();
+    fun put(key: String?, value: String?) {
+        val editor = preferences.edit()
+        editor.putString(key, value)
+        editor.apply()
     }
 
-    public void put(String key, Serializable value) {
+    fun put(key: String?, value: Serializable?) {
         try {
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ObjectOutputStream oos = new ObjectOutputStream(baos);
-            oos.writeObject(value);
-            String valueBase64 = new String(Base64.encode(baos.toByteArray(), Base64.DEFAULT));
-            SharedPreferences.Editor editor = mPreferences.edit();
-            editor.putString(key, valueBase64);
-            editor.apply();
-        } catch (IOException e) {
-            e.printStackTrace();
+            val baos = ByteArrayOutputStream()
+            val oos = ObjectOutputStream(baos)
+            oos.writeObject(value)
+            val valueBase64 = String(Base64.encode(baos.toByteArray(), Base64.DEFAULT))
+            val editor = preferences.edit()
+            editor.putString(key, valueBase64)
+            editor.apply()
+        } catch (e: IOException) {
+            e.printStackTrace()
         }
     }
 
-    public int getInt(String key, int defValue) {
-        return mPreferences.getInt(key, defValue);
-
+    fun getInt(key: String?, defValue: Int): Int {
+        return preferences.getInt(key, defValue)
     }
 
-    public boolean getBoolean(String key, boolean defValue) {
-        return mPreferences.getBoolean(key, defValue);
+    fun getBoolean(key: String?, defValue: Boolean): Boolean {
+        return preferences.getBoolean(key, defValue)
     }
 
-    public long getLong(String key, long defValue) {
-        return mPreferences.getLong(key, defValue);
+    fun getLong(key: String?, defValue: Long): Long {
+        return preferences.getLong(key, defValue)
     }
 
-    public float getFloat(String key, float defValue) {
-        return mPreferences.getFloat(key, defValue);
+    fun getFloat(key: String?, defValue: Float): Float {
+        return preferences.getFloat(key, defValue)
     }
 
-    /**
-     * 如果获取的值非法则返回默认值
-     * */
-    public double getDouble(String key, double defValue) {
-        String valueString = mPreferences.getString(key, String.valueOf(defValue));
-        try {
-            return Double.parseDouble(valueString);
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-            return defValue;
+    fun getDouble(key: String?, defValue: Double): Double {
+        val valueString = preferences.getString(key, defValue.toString())
+        return try {
+            valueString!!.toDouble()
+        } catch (e: NumberFormatException) {
+            e.printStackTrace()
+            defValue
         }
     }
 
-    public String getString(String key, String defValue) {
-        return mPreferences.getString(key, defValue);
+    fun getString(key: String?, defValue: String?): String? {
+        return preferences.getString(key, defValue)
     }
 
-    @SuppressWarnings("unchecked")
-    public <T extends Serializable> T getSerializable(String key) {
-        String objBase64 = mPreferences.getString(key, null);
-        if (objBase64 != null && !objBase64.equals("")) {
+    @Suppress("UNCHECKED_CAST")
+    fun <T : Serializable?> getSerializable(key: String?): T? {
+        val objBase64 = preferences.getString(key, null)
+        if (!objBase64.isNullOrEmpty()) {
             try {
-                byte[] base64 = Base64.decode(objBase64, Base64.DEFAULT);
-                ByteArrayInputStream bais = new ByteArrayInputStream(base64);
-                ObjectInputStream ois = new ObjectInputStream(bais);
-                Object obj = ois.readObject();
-                return (T) obj;
-            } catch (IOException | ClassNotFoundException e) {
-                e.printStackTrace();
+                val base64 = Base64.decode(objBase64, Base64.DEFAULT)
+                val bais = ByteArrayInputStream(base64)
+                val ois = ObjectInputStream(bais)
+                val obj = ois.readObject()
+                return obj as T
+            } catch (e: IOException) {
+                e.printStackTrace()
+            } catch (e: ClassNotFoundException) {
+                e.printStackTrace()
             }
         }
-        return null;
+        return null
     }
 }
