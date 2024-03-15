c977dd9644e50d9d00091073976d240017466316
lx8421bcd <lx8421bcd@github.com>
2024-03-15 17:59:50 +0800

    refactor: NumberUtil convert to kotlin impl

--- framework/src/main/java/com/linxiao/framework/common/NumberFunctions.kt
+++ framework/src/main/java/com/linxiao/framework/common/NumberFunctions.kt
@@ -1,133 +1,118 @@
-package com.linxiao.framework.common;
+package com.linxiao.framework.common
 
-import java.math.BigDecimal;
-import java.text.DecimalFormat;
+import java.math.BigDecimal
+import java.text.DecimalFormat
+import kotlin.math.abs
 
 /**
- * <p>
+ *
+ *
  * 数字处理工具合集
- * </p>
+ *
  *
  * @author linxiao
  * @since 2020-06-08
  */
-public final class NumberUtil {
 
-    /**
-     * safe parse string to number type method
-     * @param numStr number string
-     * @param def return value when parse failed
-     * @return parsed number or default value
-     */
-    public static int optInt(String numStr, int def) {
-        if (numStr == null || numStr.isEmpty()) {
-            return def;
-        }
-        try {
-            return (int) Double.parseDouble(numStr);
-        }
-        catch (Exception e) {
-            return def;
-        }
+/**
+ * safe parse string to number type method
+ * @param def return value when parse failed
+ * @return parsed number or default value
+ */
+fun String?.optInt(def: Int): Int {
+    return if (this.isNullOrEmpty()) {
+        def
+    } else try {
+        this.toDouble().toInt()
+    } catch (e: Exception) {
+        def
     }
+}
 
-    /**
-     * safe parse string to number type method
-     * @param numStr number string
-     * @param def return value when parse failed
-     * @return parsed number or default value
-     */
-    public static long optLong(String numStr, long def) {
-        if (numStr == null || numStr.isEmpty()) {
-            return def;
-        }
-        try {
-            return (long) Double.parseDouble(numStr);
-        }
-        catch (Exception e) {
-            return def;
-        }
+/**
+ * safe parse string to number type method
+ * @param def return value when parse failed
+ * @return parsed number or default value
+ */
+fun String?.optLong(def: Long): Long {
+    return if (this.isNullOrEmpty()) {
+        def
+    } else try {
+        this.toDouble().toLong()
+    } catch (e: Exception) {
+        def
     }
+}
 
-    /**
-     * safe parse string to number type method
-     * @param numStr number string
-     * @param def return value when parse failed
-     * @return parsed number or default value
-     */
-    public static float optFloat(String numStr, float def) {
-        if (numStr == null || numStr.isEmpty()) {
-            return def;
-        }
-        try {
-            return Float.parseFloat(numStr);
-        }
-        catch (Exception e) {
-            return def;
-        }
+/**
+ * safe parse string to number type method
+ * @param def return value when parse failed
+ * @return parsed number or default value
+ */
+fun String?.optFloat(def: Float): Float {
+    return if (this.isNullOrEmpty()) {
+        def
+    } else try {
+        this.toFloat()
+    } catch (e: Exception) {
+        def
     }
+}
 
-    /**
-     * safe parse string to number type method
-     * @param numStr number string
-     * @param def return value when parse failed
-     * @return parsed number or default value
-     */
-    public static double optDouble(String numStr, double def) {
-        if (numStr == null || numStr.isEmpty()) {
-            return def;
-        }
-        try {
-            return Double.parseDouble(numStr);
-        }
-        catch (Exception e) {
-            return def;
-        }
+/**
+ * safe parse string to number type method
+ * @param def return value when parse failed
+ * @return parsed number or default value
+ */
+fun String?.optDouble(def: Double): Double {
+    return if (this.isNullOrEmpty()) {
+        def
+    } else try {
+        this.toDouble()
+    } catch (e: Exception) {
+        def
     }
+}
 
-    /**
-     * get number string using wan unit(ten thousand)
-     * <p>
-     * if input num < 10000, return num
-     * if input num > 10000, return num / 10000 + unit（万/W）
-     * </p>
-     * @param num input number
-     * @param unit unit
-     * @return num string
-     */
-    public static String getWanUnitString(double num , String unit) {
-        String uc;
-        if (unit == null || unit.isEmpty()) {
-            unit = "w";
-        }
-        if (Math.abs(num) < 10000) {
-            uc = String.valueOf(num);
+/**
+ * get number string using wan unit(ten thousand)
+ *
+ *
+ * if input num < 10000, return num
+ * if input num > 10000, return num / 10000 + unit（万/W）
+ *
+ * @param unit unit
+ * @return num string
+ */
+fun Number.toWanUnitString(unit: String = "w"): String {
+    val num = this.toDouble()
+    var uc: String
+    if (abs(num) < 10000) {
+        uc = num.toString()
+    } else {
+        val value = (num / 10000.0).toFloat()
+        if (value >= 100.0 || num % 10000 == 0.0) {
+            uc = Math.round((num / 10000.0).toFloat()).toString() + unit
         } else {
-            float value = (float) (num / 10000.0);
-            if (value >= 100.0 || num % 10000 == 0) {
-                uc = Math.round((float) (num / 10000.0)) + unit;
+            var bd = BigDecimal(value.toDouble())
+            bd = bd.setScale(1, BigDecimal.ROUND_DOWN)
+            uc = bd.toString() + ""
+            uc = if (uc.endsWith(".0")) {
+                Math.round(value).toString() + unit
             } else {
-                BigDecimal bd = new BigDecimal(value);
-                bd = bd.setScale(1, BigDecimal.ROUND_DOWN);
-                uc = bd + "";
-                if (uc.endsWith(".0")) {
-                    uc = Math.round(value) + unit;
-                } else {
-                    uc = uc + unit;
-                }
+                uc + unit
             }
         }
-        return uc;
     }
+    return uc
+}
 
-    /**
-     * get thousand separator formatted style number string
-     * @param num input number
-     * @return formatted string
-     */
-    public static String getThousandSeparatorFormat(double num) {
-        DecimalFormat decimalFormat = new DecimalFormat(",###");
-        return decimalFormat.format(num);
-    }
-
+/**
+ * get thousand separator formatted style number string
+ * @param num input number
+ * @return formatted string
+ */
+fun Number.thousandSeparatorFormat(): String {
+    val decimalFormat = DecimalFormat(",###")
+    return decimalFormat.format(this.toDouble())
 }
