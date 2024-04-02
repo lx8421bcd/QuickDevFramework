380830f1e52ee5a800fbcc0269f175f4dd34fbb4
lx8421bcd <lx8421bcd@github.com>
2024-04-02 17:59:50 +0800

    feature: merge FileSizeUtil to FileUtil

--- framework/src/main/java/com/linxiao/framework/file/FileUtil.kt
+++ framework/src/main/java/com/linxiao/framework/file/FileUtil.kt
@@ -11,14 +11,17 @@ import java.io.FileWriter
 import java.io.IOException
 import java.io.InputStreamReader
 import java.nio.charset.Charset
-import java.nio.charset.StandardCharsets
+
+enum class FileSizeUnit(
+    value: Int
+) {
+    B(1),
+    KB(2),
+    MB(3),
+    GB(4)
+}
 
 /**
- * file operate methods collection
- *
- *
- * Android storage and file operate methods
- *
  *
  * @author linxiao
  * @since 2019-12-16
@@ -26,6 +29,10 @@ import java.nio.charset.StandardCharsets
 object FileUtil {
     private val TAG = FileUtil::class.java.getSimpleName()
 
+    const val KB_BYTES: Long = 1024
+    const val MB_BYTES = KB_BYTES * 1024
+    const val GB_BYTES = MB_BYTES * 1024
+    
     /**
      * check phone has external storage
      *
@@ -197,5 +204,156 @@ object FileUtil {
             return true
         }
         return path.contains(globalContext.filesDir.path)
+    }
+
+    /**
+     * get file size value by input size unit
+     *
+     * @param size     file size
+     * @param sizeType one of the defined SIZE_UNIT type in this class
+     * @return formatted value
+     */
+    fun getSizeValueByUnit(size: Long, unit: FileSizeUnit): Double {
+        return when (unit) {
+            FileSizeUnit.B -> size.toDouble()
+            FileSizeUnit.KB -> Math.round(size * 1.0 / KB_BYTES * 100) * 1.0 / 100
+            FileSizeUnit.MB -> Math.round(size * 1.0 / MB_BYTES * 100) * 1.0 / 100
+            FileSizeUnit.GB -> Math.round(size * 1.0 / GB_BYTES * 100) * 1.0 / 100
+        }
+    }
+
+    /**
+     * get formatted file size string
+     *
+     *
+     *
+     * convert following these rules: <br></br>
+     * fileSize < 0:            wrong value, output "wrong$input_number" <br></br>
+     * fileSize < 1KB:          output "fileSizeB"                       <br></br>
+     * 1KB <= fileSize < 1MB:   output "fileSizeKB"                      <br></br>
+     * 1MB <= fileSize < 1GB:   output "fileSizeMB"                      <br></br>
+     * fileSize >= 1GB:         output "fileSizeGB"                      <br></br>
+     *
+     *
+     * @param fileSize file size value, byte unit
+     * @return formatted file size string
+     */
+    @JvmStatic
+    fun getFormattedSizeString(fileSize: Long): String {
+        if (fileSize < 0) {
+            return "wrong$fileSize"
+        }
+        if (fileSize < KB_BYTES) {
+            return fileSize.toString() + "B"
+        }
+        if (fileSize < MB_BYTES) {
+            return getSizeValueByUnit(fileSize, FileSizeUnit.KB).toString() + "KB"
+        }
+        return if (fileSize < GB_BYTES) {
+            getSizeValueByUnit(fileSize, FileSizeUnit.MB).toString() + "MB"
+        } else {
+            getSizeValueByUnit(fileSize, FileSizeUnit.GB).toString() + "GB"
+        }
+    }
+
+    /**
+     * get formatted file size string for input file or directory
+     *
+     * @param file file or directory
+     * @return formatted file size string
+     */
+    fun getFileOrDirectorySizeString(file: File): String {
+        var blockSize: Long = 0
+        try {
+            blockSize = if (file.isDirectory()) {
+                getDirectorySize(file)
+            } else {
+                getFileSize(file)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return getFormattedSizeString(blockSize)
+    }
+
+    /**
+     * get file or directory size on disk
+     *
+     * @param file     file
+     * @param sizeType one of the defined SIZE_UNIT type in this class
+     * @return file size value
+     */
+    @JvmStatic
+    fun calculateSize(file: File, unit: FileSizeUnit): Double {
+        var blockSize: Long = 0
+        try {
+            blockSize = if (file.isDirectory()) {
+                getDirectorySize(file)
+            } else {
+                getFileSize(file)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return getSizeValueByUnit(blockSize, unit)
+    }
+
+    /**
+     * get total file count in input directory
+     *
+     *
+     *
+     * if input file object is not directory, method will returns 1
+     *
+     * @param file input directory
+     * @return total file count in the directory
+     */
+    @JvmStatic
+    fun calculateSubFileCount(file: File): Long {
+        if (!file.exists()) {
+            return 0
+        }
+        if (!file.isDirectory()) {
+            return 1
+        }
+        var filesSum: Long = 0
+        val fList = file.listFiles() ?: return 0
+        for (value in fList) {
+            if (value.isDirectory()) {
+                filesSum = filesSum + calculateSubFileCount(value)
+            } else {
+                filesSum++
+            }
+        }
+        return filesSum
+    }
+
+    @Throws(IOException::class)
+    private fun getFileSize(file: File?): Long {
+        if (file == null) {
+            return 0
+        }
+        var size: Long = 0
+        if (file.exists()) {
+            val fis: FileInputStream
+            fis = FileInputStream(file)
+            size = fis.available().toLong()
+            fis.close()
+        }
+        return size
+    }
+
+    @Throws(Exception::class)
+    private fun getDirectorySize(f: File): Long {
+        var size: Long = 0
+        val fList = f.listFiles() ?: return 0
+        for (file in fList) {
+            size = if (file.isDirectory()) {
+                size + getDirectorySize(file)
+            } else {
+                size + getFileSize(file)
+            }
+        }
+        return size
     }
 }
